9291f2a4ce2b27470d33578bf3e91d4a66b9937f
Atsighi Bright <brytebee@gmail.com>
2022-06-29 09:37:21 +0100

    :bug: Fixed the bug on submissions of users to the database

--- src/components/forms/Register.js
+++ src/components/forms/Register.js
@@ -16,7 +16,7 @@ const Register = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
+    console.log({ state });
     dispatch(registerUserToApi(state));
   };
 
--- src/redux/forms/userReducer.js
+++ src/redux/forms/userReducer.js
@@ -1,32 +1,43 @@
-export const registerUserToApi = (userData) => (dispatch) => {
-  fetch('https://optimistic-food.herokuapp.com/api/v1/users', {
-    method: 'POST',
-    headers: {
-      'content-type': 'application/json',
-      // Accept: 'application/json',
-    },
-    body: JSON.stringify({ user: userData }),
-  })
-    .then((resp) => resp.json())
-    .then((data) => {
-      if (data.error || data.errors) {
-        const errorMsg = data.error || data.errors;
-        dispatch({ type: 'SIGNUP_FAILED', errorMsg });
-      } else {
-        localStorage.setItem('user', JSON.stringify(data));
-        localStorage.setItem('token', data.token);
-        localStorage.setItem('isLoggedIn', true);
-        // window.history.pushState({}, '', '/dashboard');
-        // window.location.reload();
-        dispatch({ type: 'SIGNUP_SUCCESS', data });
-      }
-    });
+export const registerUserToApi = (userData) => async () => {
+  console.log(userData);
+
+  // const auth
+  const regUser = await fetch(
+    // 'https://optimistic-food.herokuapp.com/api/v1/users',
+    'http://localhost:4000/api/v1/users',
+    {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        Accept: 'application/json',
+        // Authorization: {},
+      },
+      body: JSON.stringify(userData),
+    }
+  );
+  const regUserResp = await regUser.json();
+  console.log(regUserResp);
+  // .then((resp) => resp.json())
+  // .then((data) => {
+  //   if (data.error || data.errors) {
+  //     const errorMsg = data.error || data.errors;
+  //     dispatch({ type: 'SIGNUP_FAILED', errorMsg });
+  //   } else {
+  //     localStorage.setItem('user', JSON.stringify(data));
+  //     localStorage.setItem('token', data.token);
+  //     localStorage.setItem('isLoggedIn', true);
+  //     // window.history.pushState({}, '', '/dashboard');
+  //     // window.location.reload();
+  //     dispatch({ type: 'SIGNUP_SUCCESS', data });
+  //   }
+  // });
 };
 
 export const logUserToApi = (userData) => async (dispatch) => {
   const { username, password } = userData;
   const sendData = await fetch(
-    'https://optimistic-food.herokuapp.com/api/v1/login',
+    // 'https://optimistic-food.herokuapp.com/api/v1/login',
+    'http://localhost:4000/api/v1/login',
     {
       method: 'POST',
       headers: {
@@ -34,7 +45,7 @@ export const logUserToApi = (userData) => async (dispatch) => {
         // Accept: 'application/json',
       },
       body: JSON.stringify({ username, password }),
-    },
+    }
   );
   const rawData = await sendData.json();
   console.log(rawData);
@@ -53,7 +64,7 @@ export const logUserToApi = (userData) => async (dispatch) => {
 
 const userReducer = (
   state = { user: null, isLoggedIn: false, error: null },
-  action,
+  action
 ) => {
   switch (action.type) {
     case 'SIGNUP_SUCCESS':
