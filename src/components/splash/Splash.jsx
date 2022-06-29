6be044d13feb7734ee266849d2783947b30e0890
uae <aniekaudo@yahoo.com>
2022-06-29 11:42:56 +0100

    :sparkles: Splash page
    Implement splash page

--- src/components/splash/Splash.jsx
+++ src/components/splash/Splash.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import Login from '../forms/Login';
+import './Splash.css';
+import foodforall from '../../assets/image/foodforall.jpeg';
 
 const Splash = () => (
-  <div>
+  <div className="splash-container">
+    <div className="splash__image">
+      <img src={foodforall} alt="splash-logo" />
+    </div>
     <Login />
   </div>
 );
