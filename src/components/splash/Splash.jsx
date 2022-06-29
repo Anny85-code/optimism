ee4c79b66cac959d8a4d143510d9ccd347e9ce38
uae <aniekaudo@yahoo.com>
2022-06-29 13:59:24 +0100

    :lipstick: Logo
    Styled logo

--- src/components/splash/Splash.jsx
+++ src/components/splash/Splash.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-// import Login from '../forms/Login';
 import './Splash.css';
+import './Splashlogo.css';
 import foodforall from '../../assets/image/foodforall.jpeg';
 
 const Splash = () => (
@@ -9,10 +9,25 @@ const Splash = () => (
     <div className="splash__image">
       <img src={foodforall} alt="splash-logo" />
     </div>
+    <h2 className="splash-text">..Never2Late</h2>
     <div className="login btn">
       <button type="button">
-        <NavLink to="/login">Login</NavLink>
+        <NavLink to="/login" style={{ textDecoration: 'none' }}>
+          Login
+        </NavLink>
       </button>
+    </div>
+
+    <div className="splashcontainer">
+      <div className="ring">
+        </div>
+      <div className="ring">
+        </div>
+      <div className="ring">
+        </div>
+      <div className="ring">
+        </div>
+        <p>Loading....</p>
     </div>
   </div>
 );
