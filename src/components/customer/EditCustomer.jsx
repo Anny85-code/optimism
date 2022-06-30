dcef139db0967124302737edcd40f010fc52f925
Atsighi Bright <brytebee@gmail.com>
2022-06-30 14:04:38 +0100

    :heavy_plus_sign: Add new file
    Added EditCustomer.jsx

---
+++ src/components/customer/EditCustomer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EditCustomer = () => {
+  console.log(1 + 1);
+  return (
+    <div>
+      <h1>EditCustomer</h1>
+    </div>
+  );
+};
+
+export default EditCustomer;
