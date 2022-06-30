f1d3627eef3fffd18cbc86ec8bd4fa35059ef3fb
Atsighi Bright <brytebee@gmail.com>
2022-06-30 12:06:36 +0100

    :white_check_mark: Update Customer.jsx
    Render oneCustomer data

--- src/components/customer/Customer.jsx
+++ src/components/customer/Customer.jsx
@@ -1,15 +1,17 @@
 import React, { useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { getCustomerFromApi } from '../../redux/forms/customerReducer';
 import styles from './Customer.module.css';
+import { getOneCustomerFromApi } from '../../redux/forms/oneCustomerReducer';
 
 const Customer = () => {
   const dispatch = useDispatch();
+  const param = useParams();
+  const { id } = param;
   const aCustomers = useSelector((state) => state.oneCustomer);
 
   useEffect(() => {
-    dispatch(getOneCustomerFromApi());
+    dispatch(getOneCustomerFromApi(id));
   }, []);
 
   return (
