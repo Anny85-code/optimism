66b0912d4b97a0fb5b3b1d24a3662606449fa93d
Atsighi Bright <brytebee@gmail.com>
2022-06-30 19:29:23 +0100

    :white_check_mark: Update EditCustomer.jsx
    Display empty form

--- src/components/customer/EditCustomer.jsx
+++ src/components/customer/EditCustomer.jsx
@@ -1,9 +1,125 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { postCustomerToApi } from '../../redux/forms/customerReducer';
+import '../addCostumer/AddCostumer.css';
 
 const EditCustomer = () => {
+  const param = useParams();
+  const { id } = param;
+  const aCustomers = useSelector((state) => state.oneCustomer);
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [address, setAddress] = useState('');
+  const [picture, setPicture] = useState('');
+  const [isPending, setIsPending] = useState(false);
+  const dispatch = useDispatch();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const user = JSON.parse(localStorage.getItem('user'));
+    const { id } = user.user;
+    const customer = {
+      user_id: id,
+      name,
+      email,
+      phone,
+      address,
+      picture,
+    };
+    setIsPending(true);
+    dispatch(postCustomerToApi(customer));
+    setIsPending(false);
+  };
+
   return (
-    <div>
-      <h1>EditCustomer</h1>
+    <div className="form-container">
+      <h3 className="title">Add Customer</h3>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">
+            Name *
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              name="name"
+              placeholder="Name"
+              required
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">
+            Email
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              placeholder="Email"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+        </div>
+        <div className="form-group">
+          <label htmlFor="phone">
+            Phone *
+            <input
+              type="number"
+              className="form-control"
+              id="phone"
+              name="phone"
+              placeholder="Phone"
+              required
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+          </label>
+        </div>
+        <div className="form-group">
+          <label htmlFor="address">
+            Address *
+            <input
+              type="text"
+              className="form-control"
+              id="address"
+              name="address"
+              required
+              placeholder="Address"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+            />
+          </label>
+        </div>
+        <div className="form-group">
+          <label htmlFor="picture">
+            Picture
+            <input
+              type="text"
+              className="form-control"
+              id="picture"
+              name="picture"
+              placeholder="picture"
+              value={picture}
+              onChange={(e) => setPicture(e.target.value)}
+            />
+          </label>
+        </div>
+        <div className="form-group btn">
+          {!isPending && <button type="submit">Add Customer</button>}
+          {isPending && (
+            <button type="submit" disabled>
+              Adding Customer . . .
+            </button>
+          )}
+        </div>
+      </form>
     </div>
   );
 };
