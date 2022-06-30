c2179c5507f86c13bae19d006f57f05e6e1b3a95
Atsighi Bright <brytebee@gmail.com>
2022-06-30 11:25:41 +0100

    :white_check_mark: Update Customers.jsx
    Add a navlink to a single customer view

--- src/components/customer/Customers.jsx
+++ src/components/customer/Customers.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getCustomerFromApi } from '../../redux/forms/customerReducer';
 import styles from './Customer.module.css';
@@ -14,10 +15,12 @@ const Customers = () => {
   return (
     <div>
       {allCustomers.data.map((customer) => (
-        <div key={customer.id} className={styles.containa}>
-          <h3>{customer.name}</h3>
-          <p>{customer.phone}</p>
-        </div>
+        <NavLink key={customer.id} to={`/customer/${customer.id}`}>
+          <div className={styles.containa}>
+            <h3>{customer.name}</h3>
+            <p>{customer.phone}</p>
+          </div>
+        </NavLink>
       ))}
     </div>
   );
