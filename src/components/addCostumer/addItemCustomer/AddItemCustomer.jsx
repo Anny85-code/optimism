40c7d0b365b2eb9d8232306a467d5da3c21593d3
uae <aniekaudo@yahoo.com>
2022-07-01 06:45:14 +0100

    :bug: Sum in total
    Fixing a bug in add items page

--- src/components/addCostumer/addItemCustomer/AddItemCustomer.jsx
+++ src/components/addCostumer/addItemCustomer/AddItemCustomer.jsx
@@ -1,28 +1,30 @@
+// import { useState } from 'react';
+// import { toppings } from './utils/toppings';
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getItemFromApi } from '../../../redux/forms/getItemsReducer';
 import './AddItemCustomer.css';
+// import './styles.css';
 
 const AddItemCustomer = () => {
-  const allItemsCostumer = useSelector((state) => state.item);
+  let allItemsCostumer = useSelector((state) => state.item);
+  allItemsCostumer = allItemsCostumer.data;
   const dispatch = useDispatch();
-
+  const getFormattedPrice = (price) => `$${price}`;
   const [checkedState, setCheckedState] = useState(
     new Array(allItemsCostumer.length).fill(false),
   );
-
-  const getFormattedPrice = (price) => new Intl.NumberFormat('en-IN', {
-    style: 'currency',
-    currency: 'NGN',
-  }).format(price);
-
   const [total, setTotal] = useState(0);
 
   const handleOnChange = (position) => {
     /*eslint-disable*/
-  const updatedCheckedState = checkedState.map((item, index) => (index === position ? !item : item));
-   /* eslint-enable */
+    const updatedCheckedState = checkedState.map((item, index) =>
+      index === position ? !item : item
+      );
+      /* eslint-enable */
+
     setCheckedState(updatedCheckedState);
+
     const totalPrice = updatedCheckedState.reduce(
       (sum, currentState, index) => {
         if (currentState === true) {
@@ -41,31 +43,39 @@ const AddItemCustomer = () => {
   }, []);
 
   return (
-    <div className="items-costumer">
-      {allItemsCostumer.data.map(({ name, price }, index) => (
-        // eslint-disable-next-line react/no-array-index-key
-        <div key={index} className="checkbox-container">
-          <h3 className="item-costumer-name">{name}</h3>
-          <h3 className="right-section">{getFormattedPrice(price)}</h3>
-          <h3>
-            <label htmlFor={index} className="checkbox-label">
-              <input
-                className="checkbox-input"
-                type="checkbox"
-                name={name}
-                value={name}
-                id={index}
-                checked={checkedState[index]}
-                onChange={() => handleOnChange(index)}
-              />
-            </label>
-          </h3>
-        </div>
-      ))}
-      <div className="toppings-list-item">
-        <div className="left-section">Total:</div>
-        <div className="right-section">{getFormattedPrice(total)}</div>
-      </div>
+    <div className="App">
+      <h3>Select Toppings</h3>
+      <ul className="toppings-list">
+        {allItemsCostumer.map(({ name, price }, index) => {
+          console.log(1 + 1);
+          return (
+            /* eslint-disable-next-line react/no-array-index-key */
+            <li key={index}>
+              <div className="toppings-list-item">
+                <div className="left-section">
+                  <input
+                    type="checkbox"
+                    id={`custom-checkbox-${index}`}
+                    name={name}
+                    value={name}
+                    checked={checkedState[index]}
+                    onChange={() => handleOnChange(index)}
+                  />
+                  <label htmlFor={`custom-checkbox-${index}`}>{name}</label>
+                </div>
+                <div className="right-section">{getFormattedPrice(price)}</div>
+              </div>
+            </li>
+          );
+        /* eslint-disable-next-line react/no-array-index-key */
+        })}
+        <li>
+          <div className="toppings-list-item">
+            <div className="left-section">Total:</div>
+            <div className="right-section">{getFormattedPrice(total)}</div>
+          </div>
+        </li>
+      </ul>
     </div>
   );
 };
--- src/components/addCostumer/addItemCustomer/play2.js
+++ src/components/addCostumer/addItemCustomer/play2.js
@@ -1,34 +1,75 @@
-const options = [
-  { label: 'Lifestyle', value: 'lifestyle' },
-  { label: 'Area', value: 'area' },
-  { label: 'Random', value: 'random' },
-  { label: 'Comedy', value: 'comedy' },
-  { label: 'Entertainment', value: 'entertainment' },
-];
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getItemFromApi } from '../../../redux/forms/getItemsReducer';
+import './AddItemCustomer.css';
 
-const ChannelCategory = (props) => {
-  console.log(props);
+const AddItemCustomer = () => {
+  const allItemsCostumer = useSelector((state) => state.item);
+  const dispatch = useDispatch();
+
+  const [checkedState, setCheckedState] = useState(
+    new Array(allItemsCostumer.length).fill(false)
+  );
+  const getFormattedPrice = (price) =>
+    new Intl.NumberFormat('en-IN', {
+      style: 'currency',
+      currency: 'NGN',
+    }).format(price);
+
+  const [total, setTotal] = useState(0);
+
+  const handleOnChange = (position) => {
+    /*eslint-disable*/
+    const updatedCheckedState = checkedState.map((item, index) =>
+      index === position ? !item : item
+    );
+    /* eslint-enable */
+    setCheckedState(updatedCheckedState);
+    const totalPrice = updatedCheckedState.reduce(
+      (sum, currentState, index) => {
+        if (currentState === true) {
+          return sum + allItemsCostumer[index].price;
+        }
+        return sum;
+      },
+      0
+    );
+
+    setTotal(totalPrice);
+  };
+
+  useEffect(() => {
+    dispatch(getItemFromApi());
+  }, []);
+
   return (
-    <div>
-      {props.title}
-      <ul>
-        {props.options.map((option) => (
-          <li key={props.key}>
-            <label>
-              {option.label}
+    <div className="items-costumer">
+      {allItemsCostumer.data.map(({ name, price }, index) => (
+        // eslint-disable-next-line react/no-array-index-key
+        <div key={index} className="checkbox-container">
+          <h3 className="item-costumer-name">{name}</h3>
+          <h3 className="right-section">{getFormattedPrice(price)}</h3>
+          <h3>
+            <label htmlFor={index} className="checkbox-label">
               <input
-                className={props.className}
-                name={option.value}
-                checked={props.checked.includes(option.value)}
-                onChange={(e) => props.onChange(e.target.checked, option.value)}
+                className="checkbox-input"
                 type="checkbox"
+                name={name}
+                value={name}
+                id={index}
+                checked={checkedState[index]}
+                onChange={() => handleOnChange(index)}
               />
             </label>
-          </li>
-        ))}
-      </ul>
+          </h3>
+        </div>
+      ))}
+      <div className="toppings-list-item">
+        <div className="left-section">Total:</div>
+        <div className="right-section">{getFormattedPrice(total)}</div>
+      </div>
     </div>
   );
 };
 
-export default ChannelCategory;
+export default AddItemCustomer;
