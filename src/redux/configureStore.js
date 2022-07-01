39feac09c8e728a958c38ef4f8ae0ffbf9cbd30d
Atsighi Bright <brytebee@gmail.com>
2022-07-01 05:16:42 +0100

    :white_check_mark: Update configureStore.js
    Add userManReducer to store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -8,13 +8,14 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import userReducer from './forms/userReducer';
 import customerReducer from './forms/customerReducer';
-/* eslint-disable */
 import oneCustomerReducer from './forms/oneCustomerReducer';
-/* eslint-enable */
+import userManReducer from './forms/userManReducer';
+
 const rootReducer = combineReducers({
   user: userReducer,
   customer: customerReducer,
   oneCustomer: oneCustomerReducer,
+  userManReducer,
 });
 
 const store = createStore(rootReducer, compose(applyMiddleware(logger, thunk)));
