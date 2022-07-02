b86bcc9ba5864f5b78d380db3fafac3d76ed110c
Atsighi Bright <brytebee@gmail.com>
2022-07-02 16:28:45 +0100

    :white_check_mark: Update AddItemCustomer.jsx
    Checks and addition of amount works

--- src/components/addCostumer/addItemCustomer/AddItemCustomer.jsx
+++ src/components/addCostumer/addItemCustomer/AddItemCustomer.jsx
@@ -1,98 +1,116 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-// const fs = require('fs');
 import { getItemFromApi } from '../../../redux/forms/getItemsReducer';
-import topp from '../../../assets/json/data.json';
-import './AddItemCustomer.css';
+// import topp from '../../../assets/json/data.json';
+// import './AddItemCustomer.css';
+import './play.css';
 
-const getFormattedPrice = (price) => `$${price.toFixed(2)}`;
+// export default function AddItemCustomer() {
+//   const allItemsCostumer = useSelector((state) => state.item);
+//   const [quantity, setQuantity] = useState(1);
+//   const [checked, setChecked] = useState([]);
+//   const dispatch = useDispatch();
+//   const { data } = allItemsCostumer;
 
-export default function AddItemCustomer() {
+//   useEffect(() => {
+//     dispatch(getItemFromApi());
+//   }, []);
+
+//   const handleItemQty = (e) => {
+//     if (e.target.value > 0) {
+//       setQuantity(e.target.value);
+//     }
+//   };
+
+//   const handleCheck = (e) => {
+//     let updatedList = [...checked];
+//     if (e.target.checked) {
+//       updatedList = [...checked, e.target.value];
+//     } else {
+//       updatedList.splice(checked.indexOf(e.target.value), 1);
+//     }
+//     setChecked(updatedList);
+//   };
+
+//   const checkedItems = checked.length
+//     ? checked.reduce((total, item, index, array) => {
+//         console.log(total, item, index, array);
+//         return total + ',' + item;
+//       })
+//     : '';
+
+//   let isChecked = (item) =>
+//     checked.includes(item) ? 'checked-item' : 'not-checked-item';
+
+//   return (
+//     <div>
+//       {data.map((item) => (
+//         <div key={item.id}>
+//           <input type="checkbox" onChange={handleCheck} />
+//           <h2 className={isChecked(item.name)}>{item.name}</h2>
+//           <input
+//             type="number"
+//             value={quantity}
+//             onChange={handleItemQty}
+//             style={{ width: '40px', display: 'inline' }}
+//           />
+//         </div>
+//       ))}
+
+//       <h1>{`Items checked are: ${checkedItems}`}</h1>
+//     </div>
+//   );
+// }
+
+const AddItemCustomer = () => {
+  const [checked, setChecked] = useState([]);
   const allItemsCostumer = useSelector((state) => state.item);
+  //   const [quantity, setQuantity] = useState(1);
   const dispatch = useDispatch();
   const { data } = allItemsCostumer;
-
-  // fs.writeFile(
-  //   '../../../assets/json/data.json',
-  //   JSON.stringify(data),
-  //   (err) => {
-  //     if (err) console.log('Error writing file:', err);
-  //   }
-  // );
-
-  // const input = {
-  //   newData: [...data],
-  // };
-
-  // for (let i = 0; i < input.newData.length; i++) {
-  //   input.newData[i].price = parseFloat(input.newData[i].price);
-  // }
-
-  // console.log(topp, data);
+  const dataPrices = data.map((la) => la.price);
 
   useEffect(() => {
     dispatch(getItemFromApi());
   }, []);
 
-  const [checkedState, setCheckedState] = useState(
-    new Array(topp.length).fill(false)
-  );
-
-  const [total, setTotal] = useState(0);
-
-  const handleOnChange = (position) => {
-    const updatedCheckedState = checkedState.map((item, index) =>
-      index === position ? !item : item
-    );
-
-    setCheckedState(updatedCheckedState);
-
-    const totalPrice = updatedCheckedState.reduce(
-      (sum, currentState, index) => {
-        if (currentState === true) {
-          return sum + topp[index].price;
-        }
-        return sum;
-      },
-      0
-    );
-
-    setTotal(totalPrice);
+  const handleCheck = (event) => {
+    var updatedList = [...checked];
+    if (event.target.checked) {
+      updatedList = [...checked, event.target.value];
+    } else {
+      updatedList.splice(checked.indexOf(event.target.value), 1);
+    }
+    setChecked(updatedList);
   };
 
-  console.log(total);
+  const checkedItems = checked.length
+    ? checked.reduce((total, item, ind) => {
+        return total + +dataPrices[ind];
+      }, 0)
+    : 0;
+
+  // Return classes based on whether item is checked
+  var isChecked = (item) =>
+    checked.includes(item) ? 'checked-item' : 'not-checked-item';
 
   return (
-    <div className="App">
-      <h3>Select Toppings</h3>
-      <ul className="toppings-list">
-        {topp.map(({ name, price }, index) => {
-          return (
-            <li key={index}>
-              <div className="toppings-list-item">
-                <div className="left-section">
-                  <input
-                    type="checkbox"
-                    id={`custom-checkbox-${index}`}
-                    name={name}
-                    value={name}
-                    checked={checkedState[index]}
-                    onChange={() => handleOnChange(index)}
-                  />
-                  <label htmlFor={`custom-checkbox-${index}`}>{name}</label>
-                </div>
-                <div className="right-section">{getFormattedPrice(price)}</div>
-              </div>
-            </li>
-          );
-        })}
-        <li>
-          <div className="toppings-list-item">
-            <div className="left-section">Total:</div>
-            <div className="right-section">{getFormattedPrice(total)}</div>
-          </div>
-        </li>
-      </ul>
+    <div className="app">
+      <div className="checkList">
+        <div className="title">Your CheckList:</div>
+        <div className="list-container">
+          {data.map((item, index) => (
+            <div key={index}>
+              <input value={item} type="checkbox" onChange={handleCheck} />
+              <span className={isChecked(item)}>{item.name}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div>{`Items checked are: ${checkedItems}`}</div>
     </div>
   );
-}
+};
+
+export default AddItemCustomer;
