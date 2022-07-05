79cfaaa5493a75d2a0387c594136936b3a68fe40
uae <aniekaudo@yahoo.com>
2022-07-05 13:24:22 +0100

    :lipstick: Items page
    Styled itesm page

--- src/components/addCostumer/addItemCustomer/AddItemCustomer.jsx
+++ src/components/addCostumer/addItemCustomer/AddItemCustomer.jsx
@@ -43,8 +43,55 @@ const AddItemCustomer = () => {
   };
 
   return (
-    <div className="App">
-      <h3>Select Product</h3>
+    <div className="items-costumer">
+      <h3 className="head-text">Select Product</h3>
+      {/* <ul className=""> */}
+        {data.map(({ name, price }, index) => {
+          return (
+            <div className="checkbox-container">
+              <h3>
+                {/* <li key={index}> */}
+                <label htmlFor={index} key={index} className="checkbox-label">
+                  {/* <div className="toppings-list-item"> */}
+                  {/* <div className="left-section"> */}
+                  <input
+                    type="checkbox"
+                    id={`custom-checkbox-${index}`}
+                    name={name}
+                    value={name}
+                    className="checkbox-input"
+                    checked={checkedState[index]}
+                    onChange={() => handleOnChange(index)}
+                  />
+                </label>
+              </h3>
+              <div className="price-name">
+                <h3>
+                  <label
+                    htmlFor={`custom-checkbox-${index}`}
+                    id="item-costumer-name"
+                  >
+                    {name}
+                  </label>
+                </h3>
+                <h3 className="right-section"> &nbsp;-&nbsp;</h3>
+                {/* </div> */}
+                <h3 className="right-section">{getFormattedPrice(price)}</h3>
+              </div>
+              {/* </div> */}
+              {/* </label> */}
+              {/* </li> */}
+              {/* </h3> */}
+            </div>
+          );
+        })}
+        <li>
+          <div className="toppings-list-item">
+            <div className="left-section">Total:</div>
+            <div className="right-section">{getFormattedPrice(total)}</div>
+          </div>
+        </li>
+      {/* </ul> */}
       <div className="form-group btn1">
         <button
           type="submit"
@@ -54,36 +101,55 @@ const AddItemCustomer = () => {
           Add Customer
         </button>
       </div>
-      <ul className="toppings-list">
-        {data.map(({ name, price }, index) => {
-          return (
-            <li key={index}>
-              <div className="toppings-list-item">
-                <div className="left-section">
-                  <input
-                    type="checkbox"
-                    id={`custom-checkbox-${index}`}
-                    name={name}
-                    value={name}
-                    checked={checkedState[index]}
-                    onChange={() => handleOnChange(index)}
-                  />
-                  <label htmlFor={`custom-checkbox-${index}`}>{name}</label>
-                </div>
-                <div className="right-section">{getFormattedPrice(price)}</div>
-              </div>
-            </li>
-          );
-        })}
-        <li>
-          <div className="toppings-list-item">
-            <div className="left-section">Total:</div>
-            <div className="right-section">{getFormattedPrice(total)}</div>
-          </div>
-        </li>
-      </ul>
     </div>
   );
 };
 /* eslint-enable */
 export default AddItemCustomer;
+
+// {/* <div className="items-costumer">
+//       {data.map(({ name, price }, index) => (
+//         /* eslint-disable */
+//         <div key={index} className="checkbox-container">
+//           <h3>
+//             <label htmlFor={index} className="checkbox-label">
+//               <input
+//                 className="checkbox-input"
+//                 type="checkbox"
+//                 value={name}
+//                 onChange={handleCheck}
+//               />
+//             </label>
+//           </h3>
+//           <div className="price-name">
+//             <h3 className={isChecked(name)} id="item-costumer-name">
+//               {name}
+//             </h3>
+//             <h3> &nbsp;-&nbsp;</h3>
+//             <h3 className="right-section">{getFormattedPrice(price)}</h3>
+//           </div>
+//           <div className="qty">
+//             <h2>QTY</h2>
+//             <h3>
+//               <label htmlFor={index} className="checkbox-label">
+//                 <input
+//                   className="quantity-input"
+//                   type="number"
+//                   value={quantity}
+//                   id="qauntity-input"
+//                   onChange={handleItemQty}
+//                 />
+//               </label>
+//             </h3>
+//           </div>
+//         </div>
+//       ))}
+//       <div className="toppings-list-item">
+//         <div className="left-section">Total:</div>
+//         <div className="right-section">
+//           {getFormattedPrice(checkedItems)}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };  */}
