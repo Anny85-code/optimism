4246b36741250c6614b83913ade459d889f5caf7
Atsighi Bright <brytebee@gmail.com>
2022-07-09 00:54:38 +0100

    :art: Update AddTransaction.jsx
    Add error notice when wrong ddata is supplied

--- src/components/transaction/AddTransaction.jsx
+++ src/components/transaction/AddTransaction.jsx
@@ -6,15 +6,31 @@ import { postTransactionToApi } from '../../redux/forms/transactionReducer';
 const AddTransaction = () => {
   const dispatch = useDispatch();
   const [cardNumber, setCardNumber] = useState('');
+  const error = document.getElementById('error');
 
   const getInput = (e) => {
     const input = e.target.value;
     const extractCustomerId = input.split('/')[1];
-    if (extractCustomerId.match(/[a-zA-Z]$/)) {
-      dispatch(alert('Wrong Card No.'));
-    } else {
-      setCardNumber(extractCustomerId);
+    if (extractCustomerId) {
+      if (extractCustomerId.match(/[a-zA-Z]$/)) {
+        e.target.setAttribute('disabled', true);
+        document.getElementById('resetInput').style.display = 'block';
+        error.innerText = 'Wrong Card No.\n Remove letters after /';
+        error.style.display = 'block';
+        error.style.color = 'blue';
+      } else {
+        const intId = +extractCustomerId;
+        setCardNumber(intId);
+      }
     }
+  };
+
+  const handleReset = (e) => {
+    const inputBox = document.querySelector('#card_number');
+    inputBox.disabled = false;
+    inputBox.value = '';
+    document.getElementById('resetInput').style.display = 'none';
+    error.style.display = 'none';
   };
 
   const handdleNext = () => {
@@ -28,19 +44,27 @@ const AddTransaction = () => {
   return (
     <div className="form-container">
       <h2 className="title">Collect contribution</h2>
+      <div id="error" style={{ display: 'none' }}></div>
       <form className="add-customer-form" autoComplete="off">
         <label htmlFor="name">
           Card Number *
           <input
             type="text"
             className="form-control"
-            id="card number"
+            id="card_number"
             name="card number"
             placeholder="card number"
             required
             autoCorrect="off"
             onChange={getInput}
           />
+          <span
+            id="resetInput"
+            style={{ display: 'none', color: 'white', fontSize: '12px' }}
+            onClick={handleReset}
+          >
+            Reset
+          </span>
         </label>
         <div className="form-group btn1">
           <NavLink to="/ghahja" style={{ textDecoration: 'none' }}>
