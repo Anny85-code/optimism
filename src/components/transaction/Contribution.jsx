07b23d32c190c1413dfe9dcfb0028dee67fbec5c
Atsighi Bright <brytebee@gmail.com>
2022-07-09 06:09:52 +0100

    :white_check_mark: Update Contribution
    Enter placeholder text

--- src/components/transaction/Contribution.jsx
+++ src/components/transaction/Contribution.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
 
 const Contribution = () => {
-  console.log(1 + 1);
-  return <div>Contribution</div>;
+  const handleDays = (e) => {
+    console.log(+e.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Customer Details</h2>
+      <div>
+        <p>Name:</p>
+        <p>Daily Contribution:</p>
+        <h2>Customer Details</h2>
+        <input
+          type="number"
+          className="form-control"
+          id="days_number"
+          placeholder="No. of days"
+          required
+          autoCorrect="off"
+          onChange={handleDays}
+        />
+        <p>Previous payment date:</p>
+        <p>Current payment date:</p>
+      </div>
+    </div>
+  );
 };
 
 export default Contribution;
