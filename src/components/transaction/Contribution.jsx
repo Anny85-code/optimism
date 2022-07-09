a7e82606255c6796ac10356934374783f36000f0
Atsighi Bright <brytebee@gmail.com>
2022-07-09 07:25:58 +0100

    :white_check_mark: Update Contribution.jsx
    Get payable amount from the number of days the customer want to pay

--- src/components/transaction/Contribution.jsx
+++ src/components/transaction/Contribution.jsx
@@ -1,16 +1,34 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getOneCustomerFromApi } from '../../redux/forms/OneCustomerReducer';
+/* eslint-disable */
 const Contribution = () => {
+  const dispatch = useDispatch();
+  const customerDetails = useSelector((state) => state.oneCustomer);
+  const [daysNo, setDaysNo] = useState(0);
+  const { data } = customerDetails;
   const handleDays = (e) => {
     console.log(+e.target.value);
+    setDaysNo(+e.target.value);
   };
+
+  const { cardNumber } = localStorage;
+  console.log(cardNumber);
+
+  useEffect(() => {
+    dispatch(getOneCustomerFromApi(cardNumber));
+  }, []);
+
+  const { name, daily_contribution } = data;
+  const amount = daysNo * daily_contribution;
 
   return (
     <div>
       <h2>Customer Details</h2>
+      {/* {data.map((customer) => ( */}
       <div>
-        <p>Name:</p>
-        <p>Daily Contribution:</p>
+        <p>Name: {name}</p>
+        <p>Daily Contribution: {daily_contribution}</p>
         <h2>Customer Details</h2>
         <input
           type="number"
@@ -21,11 +39,14 @@ const Contribution = () => {
           autoCorrect="off"
           onChange={handleDays}
         />
+        <p>Amount: NGN {amount}</p>
         <p>Previous payment date:</p>
         <p>Current payment date:</p>
       </div>
+      {/* ))} */}
     </div>
   );
 };
 
+/* eslint-enable */
 export default Contribution;
