396e537895329117831c65a9f17106e810ef333e
Atsighi Bright <brytebee@gmail.com>
2022-07-12 15:16:37 +0100

    :white_check_mark: Update Main.js
    Updated font-awesome to users

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -27,7 +27,8 @@ const Main = () => {
                 </div>
               </div>
               <div className="cards">
-                <i className="fa fa-calendar fa-2x text-red" />
+                {/* <i className="fa fa-calendar fa-2x text-red" /> */}
+                <i class="fa fa-users fa-2x" />
                 <div className="card__inner">
                   <p className="text-primary-p">Number of Affiliates</p>
                   <span className="font-bold text-title">2657</span>
