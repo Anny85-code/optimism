3c73cc88cc74033754927354dff2beb3ad3b461e
Atsighi Bright <brytebee@gmail.com>
2022-07-12 19:57:23 +0100

    :white_check_mark: Update AddSeason.jsx
    Added date format

--- src/components/season/AddSeason.jsx
+++ src/components/season/AddSeason.jsx
@@ -7,6 +7,31 @@ const AddSeason = () => {
   const [days, setDays] = useState(0);
   const [startDate, setStartDate] = useState(new Date());
 
+  // const { data } = customerDetails;
+  // const lastTransaction = oneCustomerTransactions.slice(-1);
+  // let lastDate;
+  // lastDate === null || lastDate === undefined
+  //   ? (lastDate = '2022-08-12') // Add start date of season here
+  //   : (lastDate = lastTransaction[0]?.current_contribution_date);
+  const date = new Date(startDate);
+  const AddDaysToDate = date.setDate(date.getDate() + days);
+  const convertDate = new Date(AddDaysToDate);
+  console.log(convertDate);
+  // const { name, daily_contribution } = data;
+  // const amount = daysNo * daily_contribution;
+  // const { user } = JSON.parse(localStorage.getItem('user'));
+
+  // const currentDate =
+  //   convertDate.getFullYear() +
+  //   '-' +
+  //   (convertDate.getMonth() + 1) +
+  //   '-' +
+  //   convertDate.getDate();
+
+  // const handleDays = (e) => {
+  //   setDaysNo(+e.target.value);
+  // };
+
   const getName = (e) => {
     setName(e.target.value);
   };
@@ -57,6 +82,7 @@ const AddSeason = () => {
         <label htmlFor="start_date">
           Start Date *
           <DatePicker
+            dateFormat="yyyy/MM/dd"
             selected={startDate}
             onChange={(date) => setStartDate(date)}
           />
