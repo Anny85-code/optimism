47cd8c1a2a4aff567ee1e9fd0657d50650df1cb3
uae <aniekaudo@yahoo.com>
2022-07-13 07:54:38 +0100

    :zap: User
    Styled user display page

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -37,21 +37,27 @@ const User = () => {
   return (
     <div className="containa">
       <div className="details-container">
-        <h3>Name: {name}</h3>
-        <p>Username: {username}</p>
-        <p>Role: {role}</p>
-        <p>Phone: {phone}</p>
-        <p>Email: {email}</p>
-        <p>Location: {location}</p>
-        <p>Address: {address}</p>
-        <p>Joined: {created_at}</p>
-        <p>Last Updated: {updated_at}</p>
+        <h3 className="cus-details">Name: {name}</h3>
+        <p className="cus-details">Username: {username}</p>
+        <p className="cus-details">Role: {role}</p>
+        <p className="cus-details">Phone: {phone}</p>
+        <p className="cus-details">Email: {email}</p>
+        <p className="cus-details">Location: {location}</p>
+        <p className="cus-details">Address: {address}</p>
+        <p className="cus-details">Joined: {created_at}</p>
+        <p className="cus-details">Last Updated: {updated_at}</p>
       </div>
       <div className="image-container">
-        <img src={avatar} alt={`${name}`} style={{ width: '100px' }} />
-        <div className="edit">
-          <NavLink to={redirect}>Edit</NavLink>
-        </div>
+        <img
+          src={avatar}
+          alt={`${name}`}
+          className="cus-image"
+        />
+      </div>
+      <div className="edit">
+        <NavLink to={redirect}>
+          <i className="fa fa-edit" />
+        </NavLink>
       </div>
     </div>
   );
