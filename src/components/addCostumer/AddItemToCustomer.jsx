2efe78383ab964618ddf83107965281fb0d2c4cd
Atsighi Bright <brytebee@gmail.com>
2022-07-15 13:28:20 +0100

    :white_check_mark: Update AddItemToCustomer.jsx
    Added logic for previous and next

--- src/components/addCostumer/AddItemToCustomer.jsx
+++ src/components/addCostumer/AddItemToCustomer.jsx
@@ -1,30 +1,53 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getItemFromApi } from '../../redux/forms/getItemsReducer';
+import { getOneItemFromApi } from '../../redux/forms/oneItemReducer';
 
 const AddItemToCustomer = () => {
   const dispatch = useDispatch();
+  const item = useSelector((state) => state.oneItem);
   const items = useSelector((state) => state.item);
-  console.log(items);
-  const funcRender = () => {
-    if (items.data) {
-      const { name, price } = items.data[0];
-      return (
-        <div>
-          <button type="button">Previous</button>
-          <div>
-            <h1>{name}</h1>
-            <h4>{price}</h4>
-          </div>
-          <button type="button">Previous</button>
-        </div>
-      );
+  const lastItem = items.data.length;
+  const [current, setCurrent] = useState(1);
+
+  const handlePrevious = () => {
+    if (current > 1) {
+      setCurrent(current - 1);
     }
   };
+
+  const handleNext = () => {
+    if (current < lastItem) {
+      setCurrent(current + 1);
+    }
+  };
+
+  console.log(current, lastItem);
+
+  const { name, price, description, picture } = item.data;
   useEffect(() => {
     dispatch(getItemFromApi());
-  }, []);
-  return <div>{items && funcRender()}</div>;
+    dispatch(getOneItemFromApi(current));
+  }, [current]);
+  return (
+    <div>
+      <div>
+        <button type="button" onClick={handlePrevious}>
+          Previous
+        </button>
+        <div>
+          <h1>{name}</h1>
+          <h4>{price}</h4>
+          <h4>{description}</h4>
+          <img src={picture} alt={name} style={{ width: '120px' }} />
+          <input type="number" onClick={handleQuantity} />
+        </div>
+        <button type="button" onClick={handleNext}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default AddItemToCustomer;
