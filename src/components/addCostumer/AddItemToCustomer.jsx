9121bcc2c22fde101487a1ae9ddf393bf755bc38
Atsighi Bright <brytebee@gmail.com>
2022-07-15 14:21:50 +0100

    :white_check_mark: Update AddItemToCustomer.jsx
    Added subTotal logic

--- src/components/addCostumer/AddItemToCustomer.jsx
+++ src/components/addCostumer/AddItemToCustomer.jsx
@@ -2,13 +2,15 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getItemFromApi } from '../../redux/forms/getItemsReducer';
 import { getOneItemFromApi } from '../../redux/forms/oneItemReducer';
-
+/* eslint-disable */
 const AddItemToCustomer = () => {
   const dispatch = useDispatch();
   const item = useSelector((state) => state.oneItem);
   const items = useSelector((state) => state.item);
   const lastItem = items.data.length;
   const [current, setCurrent] = useState(1);
+  const [qauntity, setQuantity] = useState(0);
+  const [subTotal, setSubTotal] = useState(0);
 
   const handlePrevious = () => {
     if (current > 1) {
@@ -22,13 +24,24 @@ const AddItemToCustomer = () => {
     }
   };
 
+  const handleQuantity = (e) => {
+    console.log(+e.target.value);
+    setQuantity(+e.target.value);
+  };
+
+  const handleSub = () => {
+    setSubTotal(qauntity * +price);
+  };
+
   console.log(current, lastItem);
 
   const { name, price, description, picture } = item.data;
+
   useEffect(() => {
     dispatch(getItemFromApi());
     dispatch(getOneItemFromApi(current));
   }, [current]);
+
   return (
     <div>
       <div>
@@ -40,7 +53,15 @@ const AddItemToCustomer = () => {
           <h4>{price}</h4>
           <h4>{description}</h4>
           <img src={picture} alt={name} style={{ width: '120px' }} />
-          <input type="number" onClick={handleQuantity} />
+          <input
+            type="number"
+            onChange={handleQuantity}
+            value={qauntity.toString()}
+          />
+          <button type="button" onClick={handleSub}>
+            Sum
+          </button>
+          <h3>{subTotal}</h3>
         </div>
         <button type="button" onClick={handleNext}>
           Next
@@ -49,5 +70,5 @@ const AddItemToCustomer = () => {
     </div>
   );
 };
-
+/* eslint-enable */
 export default AddItemToCustomer;
