d02d3e447ee7b49abe798fe1444058a3e71dcb0f
Atsighi Bright <brytebee@gmail.com>
2022-07-15 10:35:24 +0100

    :white_check_mark: Update AddItemToCustomer.jsx
    Fetch item and see them in the console

--- src/components/addCostumer/AddItemToCustomer.jsx
+++ src/components/addCostumer/AddItemToCustomer.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getItemFromApi } from '../../redux/forms/getItemsReducer';
 
