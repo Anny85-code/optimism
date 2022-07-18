b7c34970da75f9dcc133ac429c2f331bb0640917
Atsighi Bright <brytebee@gmail.com>
2022-07-18 23:45:30 +0100

    :rotating_light: Linters Fixed

--- src/components/season/AddSeason.jsx
+++ src/components/season/AddSeason.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import DatePicker from 'react-datepicker';
@@ -7,7 +7,6 @@ import {
   postSeasonToApi,
 } from '../../redux/forms/seasonReducer';
 import './AddSeason.css';
-import { useEffect } from 'react';
 
 /* eslint-disable */
 const AddSeason = () => {
