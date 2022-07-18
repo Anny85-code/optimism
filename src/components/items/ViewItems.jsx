f8d29197665f05ee8d19bb17337ff89d503a9ead
Atsighi Bright <brytebee@gmail.com>
2022-07-18 13:13:56 +0100

    :white_check_mark: Update ViewItems.jsx
    Page displays API data

--- src/components/items/ViewItems.jsx
+++ src/components/items/ViewItems.jsx
@@ -1,41 +1,41 @@
-import React from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { getItemFromApi } from '../../redux/forms/getItemsReducer';
-import data from '../../assets/json/data.json';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getItemFromApi } from '../../redux/forms/getItemsReducer';
 import './AddItems.css';
 
-const ViewItems = () => (
-  // const allItems = useSelector((state) => state.item);
-  // const dispatch = useDispatch();
+const ViewItems = () => {
+  const allItems = useSelector((state) => state.item);
+  const dispatch = useDispatch();
+  // const [isPending, setIsPending] = useState(false);
 
-  // useEffect(() => {
-  //   dispatch(getItemFromApi());
-  // }, []);
+  useEffect(() => {
+    dispatch(getItemFromApi());
+  }, []);
   // setIsPending(false);
-  // return
-  <div className="form-container-item-list">
-    {/* {allItems.data.map((item) => ( */}
-    {data.map((item) => (
-      <React.Fragment key={item.id}>
-        <ul className="item-name">
-          <li className="item-item-name">
-            <h3>{item.name}</h3>
-          </li>
-          <li>
-            <img
-              src={item.picture}
-              alt={item.picture}
-              style={{ width: '50px', height: '50px' }}
-            />
-          </li>
-          <li className="item-price">
-            <h3>{`NGN ${item.price}`}</h3>
-          </li>
-        </ul>
-      </React.Fragment>
-    ))}
-  </div>
-);
-// };
+
+  return (
+    <div className="form-container-item-list">
+      {allItems.data.map((item) => (
+        <div key={item.id}>
+          <ul className="item-name">
+            <li className="item-item-name">
+              <h3>{item.name}</h3>
+            </li>
+            <li>
+              <img
+                src={item.picture}
+                alt={item.picture}
+                style={{ width: '50px', height: '50px' }}
+              />
+            </li>
+            <li className="item-price">
+              <h3>{`NGN ${item.price}`}</h3>
+            </li>
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default ViewItems;
