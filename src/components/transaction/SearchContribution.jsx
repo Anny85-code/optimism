f44b7b45ec3bc569e191c183b5379f3c9a162c73
Atsighi Bright <brytebee@gmail.com>
2022-07-19 12:03:01 +0100

    :white_check_mark: Update SearchContribution.jsx
    Add placeholders to app

--- src/components/transaction/SearchContribution.jsx
+++ src/components/transaction/SearchContribution.jsx
@@ -5,6 +5,17 @@ const SearchContribution = () => {
   return (
     <div>
       <h1>SearchContribution</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa eum
+        excepturi rerum earum nam, iusto voluptas aut voluptatum, aspernatur
+        eveniet eos pariatur fugiat iste debitis deserunt recusandae impedit
+        quaerat. Totam repellat fugit amet quo cupiditate. Similique, magni eius
+        enim possimus provident cupiditate soluta veritatis, consequatur illum
+        quas sequi delectus illo iure impedit cumque odio blanditiis id
+        aspernatur dolores nobis dolorem! Laborum eveniet totam necessitatibus
+        provident laudantium vitae ut minima reiciendis nisi, ipsa praesentium
+        assumenda hic enim dolorum sunt, incidunt et?
+      </p>
     </div>
   );
 };
