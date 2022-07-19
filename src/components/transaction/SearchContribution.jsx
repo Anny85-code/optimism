4b05ea13a9ea063491a8ea669cacb4b602ce4a2c
Atsighi Bright <brytebee@gmail.com>
2022-07-19 12:01:56 +0100

    :heavy_plus_sign: Add new file
    Created SearchContribution.jsx

---
+++ src/components/transaction/SearchContribution.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SearchContribution = () => {
+  console.log('Hi');
+  return (
+    <div>
+      <h1>SearchContribution</h1>
+    </div>
+  );
+};
+
+export default SearchContribution;
