f01bde2fc1f70d8d3f86422801377b12099b05dc
Atsighi Bright <brytebee@gmail.com>
2022-07-19 12:34:30 +0100

    :white_check_mark: Update SearchContribution.jsx
    Updated component to search for contributions

--- src/components/transaction/SearchContribution.jsx
+++ src/components/transaction/SearchContribution.jsx
@@ -1,23 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import './AddTransaction.css';
+
+/* eslint-disable */
 
 const SearchContribution = () => {
-  console.log('Hi');
+  const [contNumber, setContNumber] = useState('');
+  const error = document.getElementById('error');
+
+  const getInput = (e) => {
+    const input = e.target.value;
+    const extractContributionId = input.split('/')[1];
+    if (extractContributionId) {
+      if (extractContributionId.match(/[a-zA-Z]$/)) {
+        e.target.setAttribute('disabled', true);
+        document.getElementById('resetInput').style.display = 'block';
+        error.innerText = 'Wrong No.\n Remove letters after /';
+        error.style.display = 'block';
+        error.style.color = 'blue';
+      } else {
+        const intId = +extractContributionId;
+        setContNumber(intId);
+      }
+    }
+  };
+
+  const handleReset = () => {
+    const inputBox = document.querySelector('#contribution_number');
+    inputBox.disabled = false;
+    inputBox.value = '';
+    document.getElementById('resetInput').style.display = 'none';
+    error.style.display = 'none';
+  };
+
+  const handdleNext = () => {
+    localStorage.setItem('contNumber', contNumber);
+  };
+
   return (
-    <div>
-      <h1>SearchContribution</h1>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa eum
-        excepturi rerum earum nam, iusto voluptas aut voluptatum, aspernatur
-        eveniet eos pariatur fugiat iste debitis deserunt recusandae impedit
-        quaerat. Totam repellat fugit amet quo cupiditate. Similique, magni eius
-        enim possimus provident cupiditate soluta veritatis, consequatur illum
-        quas sequi delectus illo iure impedit cumque odio blanditiis id
-        aspernatur dolores nobis dolorem! Laborum eveniet totam necessitatibus
-        provident laudantium vitae ut minima reiciendis nisi, ipsa praesentium
-        assumenda hic enim dolorum sunt, incidunt et?
-      </p>
+    <div className="form-container trans-form">
+      <h2 className="title">Search Contribution</h2>
+      <div id="error" style={{ display: 'none' }}></div>
+      <form className="add-customer-form" autoComplete="off">
+        <label htmlFor="contribution_number">
+          Contribution Number *
+          <input
+            type="text"
+            className="form-control"
+            id="contribution_number"
+            name="contribution_number"
+            placeholder="Contribution Number"
+            required
+            autoCorrect="off"
+            onChange={getInput}
+          />
+          <span
+            id="resetInput"
+            style={{ display: 'none', color: 'white', fontSize: '12px' }}
+            onClick={handleReset}
+          >
+            Reset
+          </span>
+        </label>
+        <div className="form-group btn1 trans-btn">
+          <NavLink
+            to={`/transactions/${contNumber}`}
+            style={{ textDecoration: 'none' }}
+          >
+            <button
+              type="submit"
+              className="add-trans-btn"
+              onClick={handdleNext}
+            >
+              Next
+              <i className="fa fa-arrow-right" id="toggle-btn" />
+            </button>
+          </NavLink>
+        </div>
+      </form>
     </div>
   );
 };
-
+/* eslint-enable */
 export default SearchContribution;
