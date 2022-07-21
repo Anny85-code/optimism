90489d27399212ca8f199c870624044f0c491ac3
Atsighi Bright <brytebee@gmail.com>
2022-07-21 14:46:32 +0100

    :white_check_mark: Update Transactions.jsx
    Display customer name alongside their transaction

--- src/components/transaction/Transactions.jsx
+++ src/components/transaction/Transactions.jsx
@@ -3,26 +3,43 @@ import { NavLink } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import Moment from 'moment';
 import { getTransactionFromApi } from '../../redux/forms/transactionReducer';
+import { getCustomerFromApi } from '../../redux/forms/customerReducer';
+/* eslint-disable */
 
 const Transactions = () => {
   const dispatch = useDispatch();
   const transactions = useSelector((state) => state.transactions);
+  const { customer_id } = transactions.data;
+  const customers = useSelector((state) => state.customer);
+  console.log('customers.data', customers.data);
   const data = JSON.parse(localStorage.getItem('user'));
   const { user } = data || {};
 
   useEffect(() => {
     dispatch(getTransactionFromApi());
+    dispatch(getCustomerFromApi(customer_id));
   }, []);
-  /* eslint-disable */
+
   return (
     <div>
       {transactions.data.map((transaction) => {
-        const permitted = user.role === 'admin' || transaction.user_id === user.id;
+        const permitted =
+          user.role === 'admin' || transaction.user_id === user.id;
+        const aCustomer = customers.data.filter(
+          (customer) => customer.id === transaction.customer_id
+        );
+        const { name } = aCustomer[0];
+        console.log(aCustomer);
         if (permitted) {
           return (
-            <NavLink key={transaction.id} to={`/transactions/${transaction.id}`}>
+            <NavLink
+              key={transaction.id}
+              to={`/transactions/${transaction.id}`}
+            >
               <div className="customer-container">
                 <h3>
+                  <span className="cus-name">Name:</span>
+                  {name}
                   <span className="cus-name">Amount:</span>
                   {transaction.amount}
                 </h3>
