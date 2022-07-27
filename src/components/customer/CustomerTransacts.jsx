c8d69772093686df6c5336826546ac2c254ea2b0
Atsighi Bright <brytebee@gmail.com>
2022-07-27 05:59:57 +0100

    :heavy_plus_sign: Add a new component
    Added CustomerTransacts.jsx

---
+++ src/components/customer/CustomerTransacts.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CustomerTransacts = () => {
+  console.log('CustomerTransacts');
+  return (
+    <div>
+      <h1>CustomerTransacts</h1>
+    </div>
+  );
+};
+
+export default CustomerTransacts;
