7fdfa74c7748de6e670fd52f6aaa16ef7625cee6
Atsighi Bright <brytebee@gmail.com>
2022-07-28 00:55:51 +0100

    :white_check_mark: Update MyFood.jsx
    Added placeholder

---
+++ src/components/myfood/MyFood.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+/* eslint-disable*/
+const MyFood = () => {
+  return (
+    <div>
+      <h1>MyFood</h1>
+    </div>
+  );
+};
+/* eslint-enable*/
+export default MyFood;
