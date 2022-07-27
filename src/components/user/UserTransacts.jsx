1314cc66eda029a430d6971de289b2a4736c4a05
Atsighi Bright <brytebee@gmail.com>
2022-07-28 00:15:21 +0100

    :art: Update UserTransacts.jsx
    Render transactions by date

--- src/components/user/UserTransacts.jsx
+++ src/components/user/UserTransacts.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink, useParams } from 'react-router-dom';
+import DatePicker from 'react-datepicker';
 import Moment from 'moment';
 import Loader from '../loader/Loader';
 import { getOneUserTransFromApi } from '../../redux/forms/oneUserTransactReducer';
@@ -12,10 +13,25 @@ const UserTransacts = () => {
   const transactions = useSelector((state) => state.userTransacts);
   const { data } = transactions;
   const { trans, user_name, total } = data || {};
+  const [sDate, setSDate] = useState(new Date());
+  const [filtaTrans, setFiltaTrans] = useState([]);
+  const [filtaTotal, setFiltaTotal] = useState(0);
+  const [transNo, setTransNo] = useState(0);
 
   useEffect(() => {
     dispatch(getOneUserTransFromApi(id));
   }, []);
+
+  const handleFilter = () => {
+    const selDate = sDate.toDateString();
+    const sameDay = trans.filter(
+      (el) => selDate == new Date(el.created_at).toDateString()
+    );
+    const fTotal = sameDay.reduce((a, b) => a + +b.amount, 0);
+    setFiltaTotal(fTotal);
+    setFiltaTrans(sameDay);
+    setTransNo(sameDay.length);
+  };
 
   return (
     <div>
@@ -44,24 +60,54 @@ const UserTransacts = () => {
                   <span className="cus-name">Total amount collected:</span>
                   {total}
                 </h3>
+                <label htmlFor="start_date">
+                  Start Date
+                  <DatePicker
+                    // portalId="root-portal"
+                    dateFormat="yyyy/MM/dd"
+                    selected={sDate}
+                    // showMonthDropdown
+                    // showYearDropdown
+                    dropdownMode="select"
+                    onChange={(date) => setSDate(date)}
+                    style={{ margin: 0 }}
+                  />
+                </label>
+                <button type="button" onClick={handleFilter}>
+                  Filter
+                </button>
+                <div>
+                  <p>Total by filtered date</p>
+                  <p>{filtaTotal}</p>
+                </div>
+                <div>
+                  <p>No. of Transactions</p>
+                  <p>{transNo}</p>
+                </div>
               </div>
-              {trans.map((transaction) => (
-                <NavLink
-                  key={transaction.id}
-                  to={`/transactions/${transaction.id}`}
-                >
-                  <div className="customer-container transactions-container">
-                    <h4>
-                      <span className="cus-name">Amount:</span>
-                      {transaction.amount}
-                    </h4>
-                    <h4>
-                      <span className="cus-phone">Date:</span>
-                      {Moment(transaction.created_at).format('MMMM DD, LT')}
-                    </h4>
-                  </div>
-                </NavLink>
-              ))}
+              {filtaTrans.length >= 1 ? (
+                filtaTrans.map((transaction) => (
+                  <NavLink
+                    key={transaction.id}
+                    to={`/transactions/${transaction.id}`}
+                  >
+                    <div className="customer-container transactions-container">
+                      <h4>
+                        <span className="cus-name">Amount:</span>
+                        {transaction.amount}
+                      </h4>
+                      <h4>
+                        <span className="cus-phone">Date:</span>
+                        {Moment(transaction.created_at).format('MMMM DD, LT')}
+                      </h4>
+                    </div>
+                  </NavLink>
+                ))
+              ) : (
+                <>
+                  <p>No transactions to show!</p>
+                </>
+              )}
             </>
           )}
         </>
