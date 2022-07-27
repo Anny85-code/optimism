7b69f4d927ee4e0521ff4b91e6f851e7ed8aa21b
Atsighi Bright <brytebee@gmail.com>
2022-07-27 10:11:45 +0100

    :white_check_mark: Update CustomerTransacts.jsx
    Update the link to getCustomerTransacts

--- src/components/customer/CustomerTransacts.jsx
+++ src/components/customer/CustomerTransacts.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink, useParams } from 'react-router-dom';
 import Moment from 'moment';
-import { getOneCustomerTransFromApi } from '../../redux/forms/OneCustomerReducer';
+import { getOneCustomerTransFromApi } from '../../redux/forms/oneCustomerTransactReducer';
 
 const CustomerTransacts = () => {
   const dispatch = useDispatch();
