5359e014c23e2e1553fec9219e8f77f47316ebe1
Atsighi Bright <brytebee@gmail.com>
2022-07-27 10:53:34 +0100

    :white_check_mark: Update CustomerTransactions.jsx
    Added all transactions of a chosen customer and a link to create a contribution when there's none yet.

--- src/components/customer/CustomerTransacts.jsx
+++ src/components/customer/CustomerTransacts.jsx
@@ -3,51 +3,63 @@ import { useDispatch, useSelector } from 'react-redux';
 import { NavLink, useParams } from 'react-router-dom';
 import Moment from 'moment';
 import { getOneCustomerTransFromApi } from '../../redux/forms/oneCustomerTransactReducer';
+import Loader from '../loader/Loader';
 
 const CustomerTransacts = () => {
   const dispatch = useDispatch();
   const param = useParams();
   const { id } = param;
-  const transactions = useSelector((state) => state.oneCustomer);
+  const transactions = useSelector((state) => state.customerTransactions);
   const { data } = transactions;
-  console.log(data);
 
-  // const { name } = data;
   useEffect(() => {
     dispatch(getOneCustomerTransFromApi(id));
   }, []);
 
   return (
     <div>
-      <h1>Hi</h1>
-      {data.map((el) => (
-        <div key={el.id}>
-          <p>{el.amount}</p>
-        </div>
-      ))}
+      {data ? (
+        <>
+          {data.length <= 0 ? (
+            <>
+              <p>No transactions yet!</p>
+              <button type="button">
+                <NavLink
+                  to="/addtransaction"
+                  style={{ textDecoration: 'none' }}
+                >
+                  Add New Transaction
+                </NavLink>
+              </button>
+            </>
+          ) : (
+            data.map((transaction) => (
+              <NavLink
+                key={transaction.id}
+                to={`/transactions/${transaction.id}`}
+              >
+                <div className="customer-container transactions-container">
+                  {/* <h3>
+                <span className="cus-name">Customer's Name:</span>
+                {aCustomer.name}
+              </h3> */}
+                  <h4>
+                    <span className="cus-name">Amount:</span>
+                    {transaction.amount}
+                  </h4>
+                  <h4>
+                    <span className="cus-phone">Date:</span>
+                    {Moment(transaction.created_at).format('MMMM DD, LT')}
+                  </h4>
+                </div>
+              </NavLink>
+            ))
+          )}
+        </>
+      ) : (
+        <Loader />
+      )}
     </div>
-    /*
-    <div>
-      {data.map((transaction) => (
-        <NavLink key={transaction.id} to={`/transactions/${transaction.id}`}>
-          <div className="customer-container transactions-container">
-             <h3>
-              <span className="cus-name">Customer's Name:</span>
-              {aCustomer.name}
-            </h3>
-            <h4>
-              <span className="cus-name">Amount:</span>
-              {transaction.amount}
-            </h4>
-            <h4>
-              <span className="cus-phone">Date:</span>
-              {Moment(transaction.created_at).format('MMMM DD, LT')}
-            </h4>
-          </div>
-        </NavLink>
-      ))}
-    </div>
-      */
   );
 };
 
