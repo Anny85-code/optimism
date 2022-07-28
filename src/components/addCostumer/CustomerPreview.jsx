1f0d170b26bfd8706c7be2fbca785b84c450179f
Atsighi Bright <brytebee@gmail.com>
2022-07-28 09:35:04 +0100

    :white_check_mark: Update CustomerPreview.jsx
    Render customer details and food items as well as grand total

--- src/components/addCostumer/CustomerPreview.jsx
+++ src/components/addCostumer/CustomerPreview.jsx
@@ -3,17 +3,44 @@ import React from 'react';
 // import { postMyFoodToApi } from '../../redux/forms/myFoodReducer';
 /* eslint-disable */
 const CustomerPreview = () => {
+  const customer = JSON.parse(localStorage.getItem('updated_customer'));
+  const myFood = JSON.parse(localStorage.getItem('myfood'));
+  const grandTotal = myFood.reduce((a, b) => b.subTotal + a, 0);
   const handleSubmit = () => {
     // dispatch(postCustomerToApi(userData));
     // dispatch(postMyFoodToApi(total));
   };
 
-  const customer = JSON.parse(localStorage.getItem('updated_customer'));
-  const myFood = JSON.parse(localStorage.getItem('myfood'));
+  const { address, daily_contribution, email, name, phone, picture } = customer;
+
   console.log(customer, myFood);
   return (
     <div>
-      <h1>CustomerPreview</h1>
+      <div>
+        <h1>Customer Info</h1>
+        <div>
+          <img src={picture} alt={`${name}`} style={{ width: '70px' }} />
+        </div>
+        <div>
+          <h3>Name: {name}</h3>
+          <p>Phone: {phone}</p>
+          <p>Email: {email}</p>
+          <p>Daily Contribution: NGN {daily_contribution}</p>
+          <p>Address: {address}</p>
+        </div>
+      </div>
+      <div>
+        <h1>Customer Food Items</h1>
+        {myFood.map((item) => (
+          <div key={item.id}>
+            <h3>Item: {item.name}</h3>
+            <p>Price: {item.price}</p>
+            <p>Qty: {item.qauntity}</p>
+            <p>Sub total NGN {item.subTotal}</p>
+          </div>
+        ))}
+        <p>Grand total NGN {grandTotal}</p>
+      </div>
     </div>
   );
 };
