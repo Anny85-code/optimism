7a6017e33b16e82a1aeac9cc7405c421eb2e43b7
Atsighi Bright <brytebee@gmail.com>
2022-07-29 10:24:56 +0100

    :white_check_mark: Updated CustomerPreview.jsx
    Customer's food items can be saved to the database

--- src/components/addCostumer/CustomerPreview.jsx
+++ src/components/addCostumer/CustomerPreview.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Loader from '../loader/Loader';
-// import { postCustomerToApi } from '../../redux/forms/customerReducer';
 import { postMyFoodToApi } from '../../redux/forms/myFoodReducer';
 /* eslint-disable */
 const CustomerPreview = () => {
@@ -13,21 +12,21 @@ const CustomerPreview = () => {
   const grandTotal = myFood.reduce((a, b) => b.subTotal + a, 0);
 
   const handleSubmit = () => {
-    const items = myFood.map((food) => JSON.stringify(food));
+    const myFoodObj = {};
+    myFood.map((fooda) => (myFoodObj[fooda.id] = fooda));
+
     const data = {
       customer_id: customer[0].id,
-      items,
+      items: JSON.stringify(myFoodObj),
     };
 
-    // dispatch(postCustomerToApi(customer));
-    console.log(data);
     dispatch(postMyFoodToApi(data));
     localStorage.removeItem('updated_customer');
     localStorage.removeItem('myfood');
     localStorage.removeItem('customer');
     localStorage.removeItem('image_str');
-    // window.history.pushState({}, '', '/customers');
-    // window.location.reload();
+    window.history.pushState({}, '', '/customers');
+    window.location.reload();
   };
 
   const handleCancel = () => {
--- src/components/myfood/MyFoods.jsx
+++ src/components/myfood/MyFoods.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useParams } from 'react-router-dom';
 import { getMyFoodFromApi } from '../../redux/forms/myFoodReducer';
 
 /* eslint-disable */
 const MyFoods = () => {
   const dispatch = useDispatch();
+  const param = useParams();
   const foods = useSelector((state) => state.myFood);
-  const { data } = foods;
+  const { data } = foods || {};
+  const food = data.filter((food) => food.customer_id == param.id);
+  console.log(food, data, param);
 
   useEffect(() => {
     dispatch(getMyFoodFromApi());
@@ -15,16 +18,19 @@ const MyFoods = () => {
 
   return (
     <div>
-      {data.map((food) => (
-        <NavLink key={food.id} to={`/season/${food.id}`}>
-          <div className="customer-container">
-            <h2 className="cus-name">{food.created_at}</h2>
-            <p>
-              <span>Start Date: {food.updated_at}</span>
-            </p>
-          </div>
-        </NavLink>
-      ))}
+      {food.map((food) => {
+        return (
+          <NavLink key={food.id} to={`/season/${food.id}`}>
+            <div className="customer-container">
+              <h2 className="cus-name">{food.customer_id}</h2>
+              <p className="cus-name">{food.created_at}</p>
+              <p>
+                <span>Start Date: {'items'}</span>
+              </p>
+            </div>
+          </NavLink>
+        );
+      })}
     </div>
   );
 };
