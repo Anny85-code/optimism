cd5e5e9aac94300cef8b9570fe1c647c60ce6577
uae <aniekaudo@yahoo.com>
2022-08-01 11:34:31 +0100

    :bug: Qunatiy
    Fix the less than zero select quantity bug

--- src/components/addCostumer/AddItemToCustomer.jsx
+++ src/components/addCostumer/AddItemToCustomer.jsx
@@ -12,7 +12,7 @@ const AddItemToCustomer = () => {
   const items = useSelector((state) => state.item);
   const lastItem = items.data.length;
   const [current, setCurrent] = useState(1);
-  const [qauntity, setQuantity] = useState(0);
+  const [qauntity, setQuantity] = useState(1);
   const [subTotal, setSubTotal] = useState(0);
   const [total, setTotal] = useState([]);
   const { name, price, description, picture } = item.data;
@@ -49,7 +49,8 @@ const AddItemToCustomer = () => {
   };
 
   const handleQuantity = (e) => {
-    setQuantity(+e.target.value);
+    if (e.target.value > 0){ 
+      setQuantity(+e.target.value);}
   };
 
   const handleSub = () => {
