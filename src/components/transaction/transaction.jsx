8dc62bc144bd1065028764cb5621bd7303e0f281
uae <aniekaudo@yahoo.com>
2022-08-02 02:14:38 +0530

    :bug: Transaction
    Fix tostring bug

--- src/components/transaction/transaction.jsx
+++ src/components/transaction/transaction.jsx
@@ -23,7 +23,13 @@ const Transaction = () => {
     dispatch(getOneTransactionFromApi(id));
   }, []);
 
+  
   /* eslint-disable */
+  const comma = (num) => {
+    const number = parseInt(num);
+    const newText = number.toLocaleString();
+    return newText;
+  };
 
   const {
     amount,
@@ -34,6 +40,7 @@ const Transaction = () => {
   } = transaction.data;
   const { name, picture, daily_contribution } = customer.data;
 
+  
   return (
     <div className="containa transaction">
       <div className="image-container">
@@ -42,7 +49,7 @@ const Transaction = () => {
       <div className="details-container">
         <h3 className="cus-details">Name: {name}</h3>
         <p className="cus-details">
-          Daily Contribution: NGN {daily_contribution}
+          Daily Contribution: {`NGN ${comma(daily_contribution)}`}
         </p>
         <p className="cus-details">
           Previous contribution date: {previous_contribution_date}
@@ -56,7 +63,7 @@ const Transaction = () => {
           {user.location_area.slice(0, 3).toUpperCase()}
           {user.id}/{transaction.data.id}
         </p>
-        <p className="cus-details">Amount: {amount}</p>
+        <p className="cus-details">Amount: {`NGN ${comma(amount)}`}</p>
         <p className="cus-details">
           Date of transaction:{Moment(created_at).format('MMMM DD, LT')}
         </p>
