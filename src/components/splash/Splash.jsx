045d0a69bdcf1dc8f3b0df5e59eab36ff75c5b25
uae <aniekaudo@yahoo.com>
2022-08-01 17:49:36 +0100

    :sparkles: Splash page
    Remove animation in spalsh page

--- src/components/splash/Splash.jsx
+++ src/components/splash/Splash.jsx
@@ -8,12 +8,12 @@ const Splash = () => (
   <div className="splash-container">
     <div className="splash__image">
       <img src={foodforall} alt="splash-logo" />
-      <div className="splashcontainer">
+      {/* <div className="splashcontainer">
         <div className="ring" />
         <div className="ring" />
         <div className="ring" />
         <div className="ring" />
-      </div>
+      </div> */}
     </div>
     <h2 className="splash-text">..Never2Late</h2>
     <div className="login btn1">
