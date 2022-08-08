d48e836020a4f83a13be8850724d8c35d56591f9
uae <aniekaudo@yahoo.com>
2022-08-08 10:46:55 +0100

    :lipstick: Customer page
    Update customer from row display to table

--- src/components/customer/Customers.jsx
+++ src/components/customer/Customers.jsx
@@ -15,17 +15,33 @@ const Customers = () => {
   }, []);
   /* eslint-disable */
   return (
-    <div>
-      <div className="custrans-name">
+    <div className="transact-customer-container1">
+      <div className="custrans-name1">
         <h4 className="columns">
           <span className="cus-name1">Customer's Name</span>
         </h4>
         <h4 className="columns i" id="a">
-         Phone
+          Phone
         </h4>
         <h4 className="columns">
-          <span className="cus-name1 ">Total number of customers</span>
+          <span className="cus-name2 ">Total number of customers</span>
         </h4>
+      </div>
+      <div className="custrans-name1">
+        <h3 className="columns" id="col">
+          <p className="custransactname"></p>
+        </h3>
+        <h3
+          className="columns i"
+          id="col"
+          style={{
+            borderRight: '2px solid crimson',
+            borderBottom: '2px solid crimson',
+          }}
+        ></h3>
+        <h3 className="columns" id="col">
+          {allCustomers.data.length}
+        </h3>
       </div>
 
       {allCustomers.data.map((customer) => {
@@ -33,16 +49,20 @@ const Customers = () => {
         if (permitted)
           return (
             <NavLink key={customer.id} to={`/customers/${customer.id}`}>
-              <div className="customer-container">
-                <h3>
-                  <span className="cus-name">Name:</span>
-                  {customer.name}
-                </h3>
-                <p>
-                  <span className="cus-phone">Phone:</span>
-                  {customer.phone}
-                </p>
-              </div>
+              <ul id="p-child">
+                <li>
+                  <div className="custrans-name1">
+                    <h4 className="columns" style={{color: 'crimson'}}>{customer.name}</h4>
+                    <h4
+                      className="columns i"
+                      style={{ borderRight: '2px solid crimson' }}
+                    >
+                      {customer.phone}
+                    </h4>
+                    <h4 className="columns"></h4>
+                  </div>
+                </li>
+              </ul>
             </NavLink>
           );
       })}
