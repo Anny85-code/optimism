82f9559a1b4e1f0528001c98fad4d3c7c14dc477
uae <aniekaudo@yahoo.com>
2022-08-09 17:43:43 +0100

    :sparkles: Error page
    Syled error page

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,31 +1,29 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './Error.css';
+// import error from '../assets/image/error.gif';
 
 function Error() {
   return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col">
-          <div className="d-flex align-items-center justify-content-center pt-5">
-            <div className="text-center">
-              <h1 className="display-1 fw-bold">404</h1>
-              <p className="fs-3">
-                {' '}
-                <span className="text-danger">Opps!</span>
-                {' '}
-                Page not found.
-              </p>
-              <br />
-              <p className="lead">
-                You are not authorized to view this page.
-                <br />
-                Login to view this page.
-              </p>
-              <br />
-              <NavLink to="/" className="btn btn-secondary">Go Back</NavLink>
-            </div>
-          </div>
-        </div>
+    <div className="container-error">
+      <div className="text-center">
+        <h1 className="display-1 404">404</h1>
+        <p className="fs-3">
+          <span className="text-danger">Opps!</span>
+          Page not found.
+        </p>
+
+        {/* <img src={error} alt="error" /> */}
+        <div className="image-blurred-edge" />
+        <p className="lead">
+          You are not authorized to view this page.
+          <br />
+          Login to view this page.
+        </p>
+
+        <NavLink to="/" className="btn btn-secondary">
+          Go Back
+        </NavLink>
       </div>
     </div>
   );
