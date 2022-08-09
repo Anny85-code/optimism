dad6acec7e34212440b65f629d0f8f438ded254d
uae <aniekaudo@yahoo.com>
2022-08-09 18:13:19 +0100

    :art: Error page
    Update page

--- src/components/Error.js
+++ src/components/Error.js
@@ -7,22 +7,24 @@ function Error() {
   return (
     <div className="container-error">
       <div className="text-center">
-        <h1 className="display-1 404">404</h1>
+        <h1 className="display-1 for-o-for-text">404</h1>
         <p className="fs-3">
-          <span className="text-danger">Opps!</span>
-          Page not found.
+          <span className="text-danger">Opps! Page not found.</span>
         </p>
 
         {/* <img src={error} alt="error" /> */}
         <div className="image-blurred-edge" />
-        <p className="lead">
+        <p className="text-danger">
           You are not authorized to view this page.
           <br />
           Login to view this page.
         </p>
 
         <NavLink to="/" className="btn btn-secondary">
-          Go Back
+          <div className="btn-error">
+            <i className="fa fa-arrow-left" />
+            <span>Go Back</span>
+          </div>
         </NavLink>
       </div>
     </div>
