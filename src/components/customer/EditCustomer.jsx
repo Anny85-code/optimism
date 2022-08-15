d848edc0c0098bb4f47a38dbcc3c3d199e068438
Atsighi Bright <brytebee@gmail.com>
2022-08-15 16:45:36 +0100

    :white_check_mark: Update EditCustomer.jsx
    Add conditional statements if data is unavailable

--- src/components/customer/EditCustomer.jsx
+++ src/components/customer/EditCustomer.jsx
@@ -54,124 +54,129 @@ const EditCustomer = () => {
 
   return (
     <div className="form-container edit-cus-cont">
-      {/* {name ? } */}
-      <div className="inner-container">
-        <h3 className="title">Update Customer</h3>
-      </div>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="name">
-            <span>
-              Name <span style={{ color: 'crimson' }}>*</span>
-            </span>
-            <input
-              type="text"
-              className="form-control"
-              id="name"
-              name="name"
-              placeholder="Name"
-              required
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </label>
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">
-            Email
-            <input
-              type="email"
-              className="form-control"
-              id="email"
-              name="email"
-              placeholder="Email"
-              required
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </label>
-        </div>
-        <div className="form-group">
-          <label htmlFor="phone">
-            <span>
-              Phone <span style={{ color: 'crimson' }}>*</span>
-            </span>
-            <input
-              type="number"
-              className="form-control"
-              id="phone"
-              name="phone"
-              placeholder="Phone"
-              required
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
-            />
-          </label>
-        </div>
-        <div className="form-group">
-          <label htmlFor="address">
-            <span>
-              Address <span style={{ color: 'crimson' }}>*</span>
-            </span>
-            <input
-              type="text"
-              className="form-control"
-              id="address"
-              name="address"
-              required
-              placeholder="Address"
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
-            />
-          </label>
-        </div>
-        <div className="form-group">
-          <label htmlFor="daily_contribution">
-            <span>
-              Daily Contribution <span style={{ color: 'crimson' }}>*</span>
-            </span>
-            <input
-              disabled
-              type="text"
-              className="form-control"
-              id="daily_contribution"
-              name="daily_contribution"
-              required
-              placeholder="daily Contribution"
-              value={dailyContribution}
-              onChange={(e) => setDailyContribution(e.target.value)}
-            />
-          </label>
-        </div>
-        <br />
-        <div id="img-editor">
-          <label htmlFor="picture" className="form-label reg-edit">
-            Picture
-            <div className="image-container">
-              <img src={picture} alt={`${name}`} className="cus-image" />
-            </div>
-            <span onClick={handleEditPic} className="edit-cus-icon">
-              Edit
-            </span>
-          </label>
-        </div>
-        <div id="new-img-editor" style={{ display: 'none' }}>
-          <label htmlFor="picture" className="form-label">
-            <h4 className="p-text">Picture</h4>
-            {ImageUpload()}
-          </label>
-        </div>
-        <NavLink to="/addproducts" style={{ textDecoration: 'none' }}>
-          <div className="form-group btn1 edit-cs-btn">
-            <button
-              type="submit"
-              className="btn1 btn-secondary1 add-marketer-btn update-cus-button"
-            >
-              Update Customer Items
-            </button>
+      {name ? (
+        <>
+          <div className="inner-container">
+            <h3 className="title">Update Customer</h3>
           </div>
-        </NavLink>
-      </form>
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="name">
+                <span>
+                  Name <span style={{ color: 'crimson' }}>*</span>
+                </span>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="name"
+                  name="name"
+                  placeholder="Name"
+                  required
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </label>
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">
+                Email
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  name="email"
+                  placeholder="Email"
+                  required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </label>
+            </div>
+            <div className="form-group">
+              <label htmlFor="phone">
+                <span>
+                  Phone <span style={{ color: 'crimson' }}>*</span>
+                </span>
+                <input
+                  type="number"
+                  className="form-control"
+                  id="phone"
+                  name="phone"
+                  placeholder="Phone"
+                  required
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                />
+              </label>
+            </div>
+            <div className="form-group">
+              <label htmlFor="address">
+                <span>
+                  Address <span style={{ color: 'crimson' }}>*</span>
+                </span>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="address"
+                  name="address"
+                  required
+                  placeholder="Address"
+                  value={address}
+                  onChange={(e) => setAddress(e.target.value)}
+                />
+              </label>
+            </div>
+            <div className="form-group">
+              <label htmlFor="daily_contribution">
+                <span>
+                  Daily Contribution <span style={{ color: 'crimson' }}>*</span>
+                </span>
+                <input
+                  disabled
+                  type="text"
+                  className="form-control"
+                  id="daily_contribution"
+                  name="daily_contribution"
+                  required
+                  placeholder="daily Contribution"
+                  value={dailyContribution}
+                  onChange={(e) => setDailyContribution(e.target.value)}
+                />
+              </label>
+            </div>
+            <br />
+            <div id="img-editor">
+              <label htmlFor="picture" className="form-label reg-edit">
+                Picture
+                <div className="image-container">
+                  <img src={picture} alt={`${name}`} className="cus-image" />
+                </div>
+                <span onClick={handleEditPic} className="edit-cus-icon">
+                  Edit
+                </span>
+              </label>
+            </div>
+            <div id="new-img-editor" style={{ display: 'none' }}>
+              <label htmlFor="picture" className="form-label">
+                <h4 className="p-text">Picture</h4>
+                {ImageUpload()}
+              </label>
+            </div>
+            <NavLink to="/addproducts" style={{ textDecoration: 'none' }}>
+              <div className="form-group btn1 edit-cs-btn">
+                <button
+                  type="submit"
+                  className="btn1 btn-secondary1 add-marketer-btn update-cus-button"
+                >
+                  Update Customer Items
+                </button>
+              </div>
+            </NavLink>
+          </form>
+        </>
+      ) : (
+        ''
+      )}
     </div>
   );
 };
