e07a234acbbda260f8335d45bc4284f7a8a2b663
Atsighi Bright <brytebee@gmail.com>
2022-08-23 23:56:29 +0100

    :white_check_mark: Update AddCostomer.jsx
    Update season_id for a customer

--- src/components/addCostumer/AddCostumer.jsx
+++ src/components/addCostumer/AddCostumer.jsx
@@ -17,8 +17,7 @@ const AddCustomer = () => {
   const condition3 = phone.length != 11;
   const condition4 = address === '';
   const genTruth = condition || condition3 || condition4;
-  const seasonData = seasons.data;
-  const lastSeason = seasonData[seasonData.length - 1];
+  const lastSeason = seasons?.data[seasons.data.length - 1];
 
   useEffect(() => {
     dispatch(getSeasonFromApi());
@@ -29,7 +28,7 @@ const AddCustomer = () => {
     const { id } = user.user;
     const customer = {
       user_id: id,
-      season_id: lastSeason.id,
+      season_id: lastSeason?.id,
       name,
       email,
       phone,
@@ -40,98 +39,100 @@ const AddCustomer = () => {
   };
 
   return (
-    <div className="form-container update-cus-container">
-      <div className="inner-container" id="cus-inner">
-        <h3 className="title">Add Customer</h3>
+    <div>
+      <div className="form-container update-cus-container">
+        <div className="inner-container" id="cus-inner">
+          <h3 className="title">Add Customer</h3>
+        </div>
+        <form className="add-customer-form">
+          <div className="form-group">
+            <label htmlFor="name">
+              <span>
+                Name <span style={{ color: 'crimson' }}>*</span>
+              </span>
+              <input
+                type="text"
+                className="form-control"
+                id="name"
+                name="name"
+                placeholder="Name"
+                required
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </label>
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">
+              Email
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                name="email"
+                placeholder="Email"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </label>
+          </div>
+          <div className="form-group">
+            <label htmlFor="phone">
+              <span>
+                Phone <span style={{ color: 'crimson' }}>*</span>
+              </span>
+              <input
+                type="number"
+                className="form-control"
+                id="phone"
+                name="phone"
+                placeholder="Phone"
+                required
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+              />
+            </label>
+          </div>
+          <div className="form-group">
+            <label htmlFor="address">
+              <span>
+                Address <span style={{ color: 'crimson' }}>*</span>
+              </span>
+              <input
+                type="text"
+                className="form-control"
+                id="address"
+                name="address"
+                required
+                placeholder="Address"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+              />
+            </label>
+          </div>
+          <div className="form-group-upload">
+            <label htmlFor="picture">
+              <h4 className="p-text">Picture</h4>
+              {ImageUpload()}
+            </label>
+          </div>
+          <div className="form-group-btn">
+            {!genTruth && (
+              <NavLink to="/addproducts" style={{ textDecoration: 'none' }}>
+                <button
+                  type="button"
+                  className="add-item-btn"
+                  onClick={handleAddItem}
+                >
+                  Add Items
+                  <i className="fa fa-arrow-right" id="toggle-btn" />
+                </button>
+              </NavLink>
+            )}
+          </div>
+        </form>
       </div>
-      <form className="add-customer-form">
-        <div className="form-group">
-          <label htmlFor="name">
-            <span>
-              Name <span style={{ color: 'crimson' }}>*</span>
-            </span>
-            <input
-              type="text"
-              className="form-control"
-              id="name"
-              name="name"
-              placeholder="Name"
-              required
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </label>
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">
-            Email
-            <input
-              type="email"
-              className="form-control"
-              id="email"
-              name="email"
-              placeholder="Email"
-              required
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </label>
-        </div>
-        <div className="form-group">
-          <label htmlFor="phone">
-            <span>
-              Phone <span style={{ color: 'crimson' }}>*</span>
-            </span>
-            <input
-              type="number"
-              className="form-control"
-              id="phone"
-              name="phone"
-              placeholder="Phone"
-              required
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
-            />
-          </label>
-        </div>
-        <div className="form-group">
-          <label htmlFor="address">
-            <span>
-              Address <span style={{ color: 'crimson' }}>*</span>
-            </span>
-            <input
-              type="text"
-              className="form-control"
-              id="address"
-              name="address"
-              required
-              placeholder="Address"
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
-            />
-          </label>
-        </div>
-        <div className="form-group-upload">
-          <label htmlFor="picture">
-            <h4 className="p-text">Picture</h4>
-            {ImageUpload()}
-          </label>
-        </div>
-        <div className="form-group-btn">
-          {!genTruth && (
-            <NavLink to="/addproducts" style={{ textDecoration: 'none' }}>
-              <button
-                type="button"
-                className="add-item-btn"
-                onClick={handleAddItem}
-              >
-                Add Items
-                <i className="fa fa-arrow-right" id="toggle-btn" />
-              </button>
-            </NavLink>
-          )}
-        </div>
-      </form>
     </div>
   );
 };
