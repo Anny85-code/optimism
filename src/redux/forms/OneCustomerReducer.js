94547ab597d982752bae39db598ca98fbf8d6292
Atsighi Bright <brytebee@gmail.com>
2022-08-25 16:55:11 +0100

    :white_check_mark: Update OneCustomerReducer.js
    Implement the delete reducer

--- src/redux/forms/OneCustomerReducer.js
+++ src/redux/forms/OneCustomerReducer.js
@@ -4,6 +4,9 @@ import endpoint from '../../assets/url/url';
 const GET_ONE_CUSTOMER = 'src/redux/onecustomerreducer/get_one_customer'.toUpperCase();
 const FAILED_GET_ONE_CUSTOMER = 'src/redux/onecustomerreducer/failed_get_one_customer'.toUpperCase();
 const GET_ONE_CUSTOMERS_REQUEST = 'src/redux/onecustomerreducer/get_one_customer_request'.toUpperCase();
+const DEL_ONE_CUSTOMER = 'src/redux/onecustomerreducer/del_one_customer'.toUpperCase();
+const FAILED_DEL_ONE_CUSTOMER = 'src/redux/onecustomerreducer/failed_del_one_customer'.toUpperCase();
+const DEL_ONE_CUSTOMERS_REQUEST = 'src/redux/onecustomerreducer/del_one_customer_request'.toUpperCase();
 const url = `${endpoint}/customers`;
 const { token } = localStorage;
 
@@ -23,6 +26,22 @@ const fetchOneCustomerRequest = () => ({
   error: null,
 });
 
+const delOneCustomerData = (payload) => ({
+  type: DEL_ONE_CUSTOMER,
+  payload,
+});
+
+const delOneCustomersDataFailed = (payload) => ({
+  type: FAILED_DEL_ONE_CUSTOMER,
+  payload,
+});
+
+const delOneCustomerRequest = () => ({
+  type: DEL_ONE_CUSTOMERS_REQUEST,
+  loading: true,
+  error: null,
+});
+
 export const getOneCustomerFromApi = (id) => async (dispatch) => {
   dispatch(fetchOneCustomerRequest());
   try {
@@ -34,6 +53,20 @@ export const getOneCustomerFromApi = (id) => async (dispatch) => {
     dispatch(fetchOneCustomerData(response.data));
   } catch (error) {
     dispatch(fetchOneCustomersDataFailed(error.message));
+  }
+};
+
+export const delOneCustomerFromApi = (id) => async (dispatch) => {
+  dispatch(delOneCustomerRequest());
+  try {
+    const response = await axios.get(`${url}/${id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    dispatch(delOneCustomerData(response.data));
+  } catch (error) {
+    dispatch(delOneCustomersDataFailed(error.message));
   }
 };
 
@@ -65,6 +98,8 @@ const OneCustomerReducer = (
         loading: true,
         error: null,
       };
+    case DEL_ONE_CUSTOMER:
+      return state.filter((customer) => customer.id !== action.id);
     default:
       return state;
   }
