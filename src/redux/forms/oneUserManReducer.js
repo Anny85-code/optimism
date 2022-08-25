84422e007b3b42456e240dd0683a90aabe8dd9ea
Atsighi Bright <brytebee@gmail.com>
2022-08-25 15:37:46 +0100

    :white_check_mark: Update oneUserManReducer.js
    Added the delete reducer

--- src/redux/forms/oneUserManReducer.js
+++ src/redux/forms/oneUserManReducer.js
@@ -2,8 +2,11 @@ import axios from 'axios';
 import endpoint from '../../assets/url/url';
 
 const GET_ONE_USER = 'src/redux/userreducer/get_one_user'.toUpperCase();
+const DEL_ONE_USER = 'src/redux/userreducer/del_one_user'.toUpperCase();
 const FAILED_GET_ONE_USER = 'src/redux/userreducer/failed_get_one_user'.toUpperCase();
+const FAILED_DEL_ONE_USER = 'src/redux/userreducer/failed_del_one_user'.toUpperCase();
 const GET_ONE_USER_REQUEST = 'src/redux/userreducer/get_one_user_request'.toUpperCase();
+const DEL_ONE_USER_REQUEST = 'src/redux/userreducer/del_one_user_request'.toUpperCase();
 const url = `${endpoint}/users`;
 const { token } = localStorage;
 
@@ -12,13 +15,29 @@ const fetchOneUserData = (payload) => ({
   payload,
 });
 
+const delOneUserData = (payload) => ({
+  type: DEL_ONE_USER,
+  payload,
+});
+
 const fetchOneUserDataFailed = (payload) => ({
   type: FAILED_GET_ONE_USER,
   payload,
 });
 
+const delOneUserDataFailed = (payload) => ({
+  type: FAILED_DEL_ONE_USER,
+  payload,
+});
+
 const fetchOneUserRequest = () => ({
   type: GET_ONE_USER_REQUEST,
+  loading: true,
+  error: null,
+});
+
+const delOneUserRequest = () => ({
+  type: DEL_ONE_USER_REQUEST,
   loading: true,
   error: null,
 });
@@ -34,6 +53,20 @@ export const getOneUserFromApi = (id) => async (dispatch) => {
     dispatch(fetchOneUserData(response.data));
   } catch (error) {
     dispatch(fetchOneUserDataFailed(error.message));
+  }
+};
+
+export const delOneUserFromApi = (id) => async (dispatch) => {
+  dispatch(delOneUserRequest());
+  try {
+    const response = await axios.delete(`${url}/${id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    dispatch(delOneUserData(response.data));
+  } catch (error) {
+    dispatch(delOneUserDataFailed(error.message));
   }
 };
 
@@ -65,6 +98,8 @@ const oneUserReducer = (
         loading: true,
         error: null,
       };
+    case DEL_ONE_USER:
+      return state.filter((user) => user.id !== action.id);
     default:
       return state;
   }
