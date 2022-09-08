7a619217b23e913204796641ac578b1b7807e039
Atsighi Bright <brytebee@gmail.com>
2022-09-08 10:42:01 +0100

    :white_check_mark: Update ItemsStats.jsx
    Rendered demo data to screen

--- src/components/items/products/ItemsStats.jsx
+++ src/components/items/products/ItemsStats.jsx
@@ -1,4 +1,23 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getMyFoodFromApi } from '../../../redux/forms/myFoodReducer';
+
 export const ItemsStats = () => {
-  console.log('hi');
-  return <div>ItemsStats</div>;
+  const dispatch = useDispatch();
+  const foods = useSelector((state) => state.myFood?.data);
+  console.log(foods);
+
+  useEffect(() => {
+    dispatch(getMyFoodFromApi());
+  }, []);
+
+  return (
+    <div>
+      {foods.map((each_customer) => (
+        <div key={each_customer.id}>
+          <p>{each_customer.id}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
