bd0f6d555b54d0f333941227f0b85b050026830c
Atsighi Bright <brytebee@gmail.com>
2022-09-09 16:01:24 +0100

    :white_check_mark: Update UsersMarketers.jsx
    Debugging

--- src/components/user/UsersMarketers.jsx
+++ src/components/user/UsersMarketers.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-// import '../customer/Customer.css';
 import { getUsersFromApi } from '../../redux/forms/userManReducer';
 
 /* eslint-disable */
@@ -9,8 +8,7 @@ const UsersMarketers = () => {
   const dispatch = useDispatch();
   const allUsers = useSelector((state) => state.userManReducer);
   const data = JSON.parse(localStorage.getItem('user'));
-  const { userr } = data || {};
-  console.log(userr);
+  const userr = data.user || {};
 
   useEffect(() => {
     dispatch(getUsersFromApi());
@@ -19,17 +17,17 @@ const UsersMarketers = () => {
   return (
     <div>
       {allUsers.data.map((user) => {
-        // const permitted = userr.role === 'admin' || userr.user_id === user.id;
-        // permitted && (
-        //   <NavLink key={user.id} to={`/users/${user.id}`}>
-        //     {user.role === 'marketer' && (
-        //       <div className="customer-container">
-        //         <h3>{user.name}</h3>
-        //         <p>{user.phone}</p>
-        //       </div>
-        //     )}
-        //   </NavLink>
-        // );
+        const permitted = userr.role === 'admin' || userr.user_id === user.id;
+        permitted && (
+          <NavLink key={user.id} to={`/users/${user.id}`}>
+            {user.role === 'marketer' && (
+              <div className="customer-container">
+                <h3>{user.name}</h3>
+                <p>{user.phone}</p>
+              </div>
+            )}
+          </NavLink>
+        );
       })}
     </div>
   );
