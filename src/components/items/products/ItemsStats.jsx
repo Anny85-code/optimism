47fc032d07558706ac48e0387bfbbe3d8d941eb4
Atsighi Bright <brytebee@gmail.com>
2022-09-09 09:43:19 +0100

    :white_check_mark: Update ItemsStats.jsx
    Rendered all customer items to the page

--- src/components/items/products/ItemsStats.jsx
+++ src/components/items/products/ItemsStats.jsx
@@ -2,10 +2,18 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getMyFoodFromApi } from '../../../redux/forms/myFoodReducer';
 
+/* eslint-disable */
 export const ItemsStats = () => {
   const dispatch = useDispatch();
   const foods = useSelector((state) => state.myFood?.data);
-  console.log(foods);
+  const iii = foods.map((customer) => {
+    const myFoods = Object.values(JSON.parse(customer.items));
+    return myFoods.map((food) => {
+      return [food.name, food.qauntity];
+    });
+  });
+
+  console.log(iii);
 
   useEffect(() => {
     dispatch(getMyFoodFromApi());
@@ -13,11 +21,33 @@ export const ItemsStats = () => {
 
   return (
     <div>
-      {foods.map((each_customer) => (
-        <div key={each_customer.id}>
-          <p>{each_customer.id}</p>
-        </div>
-      ))}
+      {foods.map((eachCustomer) => {
+        // if key exists?
+        // value += current value
+        // else
+        // create a new key with the current value
+        const myFoods = Object.values(JSON.parse(eachCustomer.items));
+        // myFoods.map((food) => {
+        //   if (summary[food.name]) {
+        //     summary[food.name] += food.qauntity;
+        //   } else {
+        //     summary[food.name] = food.qauntity;
+        //   }
+        // });
+        return (
+          <div key={eachCustomer.id}>
+            <p>{eachCustomer.id}</p>
+            {myFoods.map((food) => (
+              <div key={food.id}>
+                <p>
+                  {food.name} **** {food.qauntity}{' '}
+                </p>
+              </div>
+            ))}
+          </div>
+        );
+      })}
     </div>
   );
+  /* eslint-enable */
 };
---
+++ src/components/items/products/play.js
@@ -0,0 +1,54 @@
+// const obj = {};
+// const arr = [
+//   { a: 10, b: 15, c: 5 },
+//   { a: 2, b: 18, c: 8 },
+// ];
+
+// arr.map((ob) => {
+//   const ay = Object.keys(ob);
+//   // const xy = Object.values(ob);
+//   ay.map((k) => {
+//     if (k) {
+//       obj[k] += k;
+//     }
+//   });
+// });
+
+// const ui = { a: 10, b: 5 };
+
+// // console.log(Object.keys(ui));
+// console.log(obj);
+
+const arr = [
+  {
+    car: 'audi',
+    value: 5,
+  },
+  {
+    car: 'audi',
+    value: 7,
+  },
+  {
+    car: 'fiat',
+    value: 2,
+  },
+  {
+    car: 'fiat',
+    value: 8,
+  },
+];
+
+const result = Object.entries(
+  arr.reduce((acc, { car, value }) => {
+    if (acc[car]) {
+      return {
+        ...acc,
+        [car]: [...acc[car], value],
+      };
+    }
+
+    return { ...acc, [car]: [value] };
+  }, [])
+).map(([car, values]) => ({ car, values }));
+
+console.log(result);
