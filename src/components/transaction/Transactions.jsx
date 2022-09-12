ca1e8901e99d58af283f5a1b1b93317101c71b73
Atsighi Bright <brytebee@gmail.com>
2022-09-12 07:08:48 +0100

    :white_check_mark: Update Transactions.jsx
    Cleanup

--- src/components/transaction/Transactions.jsx
+++ src/components/transaction/Transactions.jsx
@@ -3,9 +3,9 @@ import { NavLink } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import Moment from 'moment';
 import { getTransactionFromApi } from '../../redux/forms/transactionReducer';
-import { getCustomerFromApi } from '../../redux/forms/customerReducer';
+// import { getCustomerFromApi } from '../../redux/forms/customerReducer';
 import './Transactions.css';
-import Loader from '../loader/Loader';
+// import Loader from '../loader/Loader';
 /* eslint-disable */
 
 const Transactions = () => {
@@ -29,87 +29,72 @@ const Transactions = () => {
   };
   return (
     <div className="transact-customer-container1">
-      {transactions ? (
-        transactions.map((_) => (
-          <div className="transact-customer-container1">
-            <div className="custrans-name1">
-              <h4 className="columns">
-                <span className="cus-name1">Customer's Name</span>
-              </h4>
-              <h4 className="columns i" id="a">
-                Amount
-              </h4>
-              <h4 className="columns">
-                <span className="cus-name2 ">Date</span>
-              </h4>
-            </div>
-            <div className="custrans-name1">
-              <h3 className="columns" id="col">
-                <p className="custransactname"></p>
-              </h3>
-              <h3
-                className="columns i"
-                id="col"
-                style={{
-                  borderRight: '2px solid crimson',
-                  borderBottom: '2px solid crimson',
-                }}
-              ></h3>
-              <h3
-                className="columns"
-                id="col"
-                style={{ color: 'crimson' }}
-              ></h3>
-            </div>
-            {transactions &&
-              transactions.map((transaction) => {
-                const permitted =
-                  user.role === 'admin' || transaction.user_id === user.id;
-                const aCustomer = {};
-                customers.data.filter((customer) => {
-                  if (customer.id === transaction.customer_id) {
-                    aCustomer.name = customer.name;
-                  }
-                });
+      <div className="transact-customer-container1">
+        <div className="custrans-name1">
+          <h4 className="columns">
+            <span className="cus-name1">Customer's Name</span>
+          </h4>
+          <h4 className="columns i" id="a">
+            Amount
+          </h4>
+          <h4 className="columns">
+            <span className="cus-name2 ">Date</span>
+          </h4>
+        </div>
+        <div className="custrans-name1">
+          <h3 className="columns" id="col">
+            <p className="custransactname"></p>
+          </h3>
+          <h3
+            className="columns i"
+            id="col"
+            style={{
+              borderRight: '2px solid crimson',
+              borderBottom: '2px solid crimson',
+            }}
+          ></h3>
+          <h3 className="columns" id="col" style={{ color: 'crimson' }}></h3>
+        </div>
+        {transactions &&
+          transactions.map((transaction) => {
+            const permitted =
+              user.role === 'admin' || transaction.user_id === user.id;
+            const aCustomer = {};
+            customers.data.filter((customer) => {
+              if (customer.id === transaction.customer_id) {
+                aCustomer.name = customer.name;
+              }
+            });
 
-                if (permitted) {
-                  return (
-                    <NavLink
-                      key={transaction.id}
-                      to={`/transactions/${transaction.id}`}
-                    >
-                      <ul id="p-child">
-                        <li>
-                          <div className="custrans-name1">
-                            <h4
-                              className="columns"
-                              style={{ color: 'crimson' }}
-                            >
-                              {aCustomer.name}
-                            </h4>
-                            <h4
-                              className="columns i"
-                              style={{ borderRight: '2px solid crimson' }}
-                            >
-                              {` NGN ${comma(transaction.amount)}`}
-                            </h4>
-                            <h4 className="columns" style={{ color: '#000' }}>
-                              {Moment(transaction.created_at).format(
-                                'MMMM DD, LT'
-                              )}
-                            </h4>
-                          </div>
-                        </li>
-                      </ul>
-                    </NavLink>
-                  );
-                }
-              })}
-          </div>
-        ))
-      ) : (
-        <Loader />
-      )}
+            if (permitted) {
+              return (
+                <NavLink
+                  key={transaction.id}
+                  to={`/transactions/${transaction.id}`}
+                >
+                  <ul id="p-child">
+                    <li>
+                      <div className="custrans-name1">
+                        <h4 className="columns" style={{ color: 'crimson' }}>
+                          {aCustomer.name}
+                        </h4>
+                        <h4
+                          className="columns i"
+                          style={{ borderRight: '2px solid crimson' }}
+                        >
+                          {` NGN ${comma(transaction.amount)}`}
+                        </h4>
+                        <h4 className="columns" style={{ color: '#000' }}>
+                          {Moment(transaction.created_at).format('MMMM DD, LT')}
+                        </h4>
+                      </div>
+                    </li>
+                  </ul>
+                </NavLink>
+              );
+            }
+          })}
+      </div>
     </div>
   );
 };
