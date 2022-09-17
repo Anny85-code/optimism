73965853f7472b153fd2f867c0256c9931b4ba09
Atsighi Bright <brytebee@gmail.com>
2022-09-17 17:07:55 +0100

    :white_check_mark: Update Transactions.jsx
    Combined admin & superadmin to admins

--- src/components/transaction/Transactions.jsx
+++ src/components/transaction/Transactions.jsx
@@ -18,6 +18,7 @@ const Transactions = () => {
   const customers = useSelector((state) => state.customer);
   const data = JSON.parse(localStorage.getItem('user'));
   const { user } = data || {};
+  const admins = user.role === 'admin' || user.role === 'superadmin';
 
   useEffect(() => {
     dispatch(getTransactionFromApi());
@@ -29,70 +30,69 @@ const Transactions = () => {
   };
   return (
     <div className="transact-customer-container1">
-        <div className="custrans-name1">
-          <h4 className="columns">
-            <span className="cus-name1">Customer's Name</span>
-          </h4>
-          <h4 className="columns i" id="a">
-            Amount
-          </h4>
-          <h4 className="columns">
-            <span className="cus-name2 ">Date</span>
-          </h4>
-        </div>
-        <div className="custrans-name1">
-          <h3 className="columns" id="col">
-            <p className="custransactname"></p>
-          </h3>
-          <h3
-            className="columns i"
-            id="col"
-            style={{
-              borderRight: '2px solid crimson',
-              borderBottom: '2px solid crimson',
-            }}
-          ></h3>
-          <h3 className="columns" id="col" style={{ color: 'crimson' }}></h3>
-        </div>
-        {transactions &&
-          transactions.map((transaction) => {
-            const permitted =
-              user.role === 'admin' || transaction.user_id === user.id;
-            const aCustomer = {};
-            customers.data.filter((customer) => {
-              if (customer.id === transaction.customer_id) {
-                aCustomer.name = customer.name;
-              }
-            });
-
-            if (permitted) {
-              return (
-                <NavLink
-                  key={transaction.id}
-                  to={`/transactions/${transaction.id}`}
-                >
-                  <ul id="p-child">
-                    <li>
-                      <div className="custrans-name1">
-                        <h4 className="columns" style={{ color: 'crimson' }}>
-                          {aCustomer.name}
-                        </h4>
-                        <h4
-                          className="columns i"
-                          style={{ borderRight: '2px solid crimson' }}
-                        >
-                          {` NGN ${comma(transaction.amount)}`}
-                        </h4>
-                        <h4 className="columns" style={{ color: '#000' }}>
-                          {Moment(transaction.created_at).format('MMMM DD, LT')}
-                        </h4>
-                      </div>
-                    </li>
-                  </ul>
-                </NavLink>
-              );
+      <div className="custrans-name1">
+        <h4 className="columns">
+          <span className="cus-name1">Customer's Name</span>
+        </h4>
+        <h4 className="columns i" id="a">
+          Amount
+        </h4>
+        <h4 className="columns">
+          <span className="cus-name2 ">Date</span>
+        </h4>
+      </div>
+      <div className="custrans-name1">
+        <h3 className="columns" id="col">
+          <p className="custransactname"></p>
+        </h3>
+        <h3
+          className="columns i"
+          id="col"
+          style={{
+            borderRight: '2px solid crimson',
+            borderBottom: '2px solid crimson',
+          }}
+        ></h3>
+        <h3 className="columns" id="col" style={{ color: 'crimson' }}></h3>
+      </div>
+      {transactions &&
+        transactions.map((transaction) => {
+          const permitted = admins || transaction.user_id === user.id;
+          const aCustomer = {};
+          customers.data.filter((customer) => {
+            if (customer.id === transaction.customer_id) {
+              aCustomer.name = customer.name;
             }
-          })}
+          });
+
+          if (permitted) {
+            return (
+              <NavLink
+                key={transaction.id}
+                to={`/transactions/${transaction.id}`}
+              >
+                <ul id="p-child">
+                  <li>
+                    <div className="custrans-name1">
+                      <h4 className="columns" style={{ color: 'crimson' }}>
+                        {aCustomer.name}
+                      </h4>
+                      <h4
+                        className="columns i"
+                        style={{ borderRight: '2px solid crimson' }}
+                      >
+                        {` NGN ${comma(transaction.amount)}`}
+                      </h4>
+                      <h4 className="columns" style={{ color: '#000' }}>
+                        {Moment(transaction.created_at).format('MMMM DD, LT')}
+                      </h4>
+                    </div>
+                  </li>
+                </ul>
+              </NavLink>
+            );
+          }
+        })}
     </div>
   );
 };
