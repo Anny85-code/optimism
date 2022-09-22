6b11639b38eb51712d6daafc1cef8fa05292222a
Atsighi Bright <brytebee@gmail.com>
2022-09-22 16:43:10 +0100

    :white_check_mark: Update Search.jsx
    Removed the filter before render

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -4,20 +4,27 @@ import { NavLink } from 'react-router-dom';
 import { getCustomerFromApi } from '../../redux/forms/customerReducer';
 import './Search.css';
 
+/* eslint-disable*/
 const Search = () => {
   const dispatch = useDispatch();
-  const allCustomers = useSelector((state) => state.customer);
+  const allCustomers = useSelector((state) => state.customer?.data);
   const [aCustomer, setACustomer] = useState('');
   const data = JSON.parse(localStorage.getItem('user'));
   const { user } = data || {};
   const useID = user.id;
 
-  /* eslint-disable*/
-  const filterCustomer = allCustomers.data.filter((customer) => {
-    return customer.user_id === useID;
+  const filterCustomer = [];
+  allCustomers.map((customer) => {
+    console.log(customer.user_id);
+    if (customer.user_id === useID) {
+      filterCustomer.push(customer);
+    }
   });
+
   const handleSearch = () => {
     document.getElementById('search-container1').style.display = 'none';
+    document.getElementById('search-input').value = '';
+    setACustomer(null);
   };
 
   const handleSelected = () => {
@@ -32,7 +39,7 @@ const Search = () => {
 
   const handleChange = (event) => {
     const typedContent = event.target.value;
-    const filteredCustomer = filterCustomer.filter((customer) =>
+    const filteredCustomer = allCustomers.filter((customer) =>
       customer.name.toLowerCase().includes(typedContent.toLowerCase())
     );
     setACustomer(filteredCustomer);
@@ -46,6 +53,7 @@ const Search = () => {
         <div className="search-inner">
           <input
             className="search-input"
+            id="search-input"
             type="text"
             onChange={handleChange}
             placeholder="Search customers"
@@ -59,7 +67,9 @@ const Search = () => {
             <NavLink key={customer.id} to={`/customers/${customer.id}`}>
               <div className="dropdown-row">
                 <div className="search-text">
-                  <p onClick={handleSelected}>{customer.name}</p>
+                  <p onClick={handleSelected}>
+                    {customer.card_number} - {customer.name}
+                  </p>
                 </div>
               </div>
             </NavLink>
