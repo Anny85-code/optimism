22f061b25dfb2ae081ca29ee599ba8a931bf8bc2
Atsighi Bright <brytebee@gmail.com>
2022-09-23 19:10:14 +0100

    :white_check_mark: Update Customer.jsx
    Added a loader

--- src/components/customer/Customers.jsx
+++ src/components/customer/Customers.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getCustomerFromApi } from '../../redux/forms/customerReducer';
 import './Customers.css';
 import { getUsersFromApi } from '../../redux/forms/userManReducer';
+import Loader from '../loader/Loader';
 /* eslint-disable */
 
 const Customers = () => {
@@ -36,58 +37,64 @@ const Customers = () => {
   });
 
   return (
-    <div className="transact-customer-container1">
-      <div className="custrans-name1">
-        <h4 className="columns">
-          <span className="cus-name1">Customer's Name</span>
-        </h4>
-        <h4 className="columns i" id="a">
-          Phone
-        </h4>
-        <h4 className="columns">
-          <span className="cus-name2 ">Total number of customers</span>
-        </h4>
-      </div>
-      <div className="custrans-name1">
-        <h3 className="columns" id="col">
-          <p className="custransactname"></p>
-        </h3>
-        <h3
-          className="columns i"
-          id="col"
-          style={{
-            borderRight: '2px solid crimson',
-            borderBottom: '2px solid crimson',
-          }}
-        ></h3>
-        <h3 className="columns" id="col" style={{ color: 'crimson' }}>
-          {admins && allCustomers.length}
-          {user.role === 'marketer' && marketerCustomer.length}
-        </h3>
-      </div>
+    <>
+      {newData ? (
+        <div className="transact-customer-container1">
+          <div className="custrans-name1">
+            <h4 className="columns">
+              <span className="cus-name1">Customer's Name</span>
+            </h4>
+            <h4 className="columns i" id="a">
+              Phone
+            </h4>
+            <h4 className="columns">
+              <span className="cus-name2 ">Total number of customers</span>
+            </h4>
+          </div>
+          <div className="custrans-name1">
+            <h3 className="columns" id="col">
+              <p className="custransactname"></p>
+            </h3>
+            <h3
+              className="columns i"
+              id="col"
+              style={{
+                borderRight: '2px solid crimson',
+                borderBottom: '2px solid crimson',
+              }}
+            ></h3>
+            <h3 className="columns" id="col" style={{ color: 'crimson' }}>
+              {admins && allCustomers.length}
+              {user.role === 'marketer' && marketerCustomer.length}
+            </h3>
+          </div>
 
-      {newData &&
-        newData.map((customer) => (
-          <NavLink key={customer.id} to={`/customers/${customer.id}`}>
-            <ul id="p-child">
-              <li>
-                <div className="custrans-name1">
-                  <h4 className="columns" style={{ color: 'crimson' }}>
-                    {customer.name}
-                  </h4>
-                  <h4
-                    className="columns i"
-                    style={{ borderRight: '2px solid crimson' }}
-                  >
-                    {customer.phone}
-                  </h4>
-                  <h4 className="columns"></h4>
-                </div>
-              </li>
-            </ul>
-          </NavLink>
-        ))}
-    </div>
+          {newData &&
+            newData.map((customer) => (
+              <NavLink key={customer.id} to={`/customers/${customer.id}`}>
+                <ul id="p-child">
+                  <li>
+                    <div className="custrans-name1">
+                      <h4 className="columns" style={{ color: 'crimson' }}>
+                        {customer.name}
+                      </h4>
+                      <h4
+                        className="columns i"
+                        style={{ borderRight: '2px solid crimson' }}
+                      >
+                        {customer.phone}
+                      </h4>
+                      <h4 className="columns"></h4>
+                    </div>
+                  </li>
+                </ul>
+              </NavLink>
+            ))}
+        </div>
+      ) : (
+        <Loader />
+      )}
+    </>
   );
 };
 /* eslint-enable */
