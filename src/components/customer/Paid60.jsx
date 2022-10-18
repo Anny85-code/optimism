9f91d2acbf8be0578e3481dbc787846a0c54f471
Atsighi Bright <brytebee@gmail.com>
2022-10-18 19:58:22 +0100

    :white_check_mark: Update Codebase
    Added route to app to paid 60%

--- src/components/customer/Paid60.jsx
+++ src/components/customer/Paid60.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const PaidSixty = () => {
-  return <div>Paid60</div>;
+  const data = JSON.parse(localStorage.getItem('user'));
+  const { user } = data || {};
+  const admins = user.role === 'admin' || user.role === 'superadmin';
+  return (
+    <div>
+      {admins && <p>Paid60</p>}
+      {!admins && <p>Unauthorized to see this page!</p>}
+    </div>
+  );
 };
 
 export default PaidSixty;
