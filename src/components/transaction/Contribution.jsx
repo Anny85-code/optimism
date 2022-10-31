13050656023610e3ac496ec252690e89351841ec
Atsighi Bright <brytebee@gmail.com>
2022-11-01 00:09:36 +0100

    :art: Update Contribution.jsx customer details

--- src/components/transaction/Contribution.jsx
+++ src/components/transaction/Contribution.jsx
@@ -19,19 +19,16 @@ const Contribution = () => {
   const transactions = useSelector((state) => state.customerTransactions?.data);
 
   const isReady = customer && transactions;
-  // const transactions = useSelector(
-  //   (state) => state.transactions?.data?.transactions
-  // );
   // const seasons = useSelector((state) => state.seasons);
   // const season = useSelector((state) => state.oneSeason?.data);
   // const { start_date } = season;
   // const lastSeason = seasons.data.length;
-  // const [go, setGo] = useState(false);
+  const [go, setGo] = useState(false);
   // const [daysNo, setDaysNo] = useState(0);
   // const [trDate, setTrDate] = useState(new Date().toLocaleDateString());
   const dispatch = useDispatch();
 
-  // const lastTransaction = transaction?.slice(-1);
+  // const lastTransaction = transactions?.slice(-1);
   // let lastDate;
   // lastTransaction.length
   //   ? (lastDate = lastTransaction[0]?.current_contribution_date)
@@ -39,7 +36,7 @@ const Contribution = () => {
   // const date = new Date(lastDate);
   // const AddDaysToDate = date.setDate(date.getDate() + daysNo);
   // const convertDate = new Date(AddDaysToDate);
-  // const { name, daily_contribution } = customer;
+  const { name, daily_contribution } = customer;
   // const amount = daysNo * daily_contribution;
   // const { user } = JSON.parse(localStorage.getItem('user'));
 
@@ -60,10 +57,10 @@ const Contribution = () => {
     }
   };
 
-  const handleRetry = () => {
-    window.pushState({}, '', '/');
-    window.location.reload();
-  };
+  // const handleRetry = () => {
+  //   window.pushState({}, '', '/');
+  //   window.location.reload();
+  // };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -90,53 +87,52 @@ const Contribution = () => {
   return (
     <div className="contribution-form">
       {isReady ? (
-        // <>
-        //   <div className="inner-container">
-        //     <h2 className="title1">Contribution details</h2>
-        //   </div>
-        //   <center style={{ color: 'crimson' }}>
-        //     Enter no. of days to continue!
-        //   </center>
-        //   <h3 className="details">Customer details</h3>
-        //   <form onSubmit={handleSubmit} className="add-customer-form">
-        //     <div className="contribution-container">
-        //       <p>Name: {name}</p>
-        //       <p>Daily Contribution: {daily_contribution}</p>
-        //       <h3 className="details t-details">Transaction Details</h3>
-        //       <input
-        //         type="number"
-        //         className="form-control days-input"
-        //         id="days_number"
-        //         placeholder="No. of days"
-        //         required
-        //         autoCorrect="off"
-        //         onChange={handleDays}
-        //       />
-        //       <input
-        //         type="date"
-        //         className="form-control days-input"
-        //         name=""
-        //         id=""
-        //         onChange={(e) => setTrDate(e.target.value)}
-        //       />
-        //       <p>Amount: NGN {amount}</p>
-        //       <p>Previous payment date: {lastDate}</p>
-        //       <p>Current payment date: {currentDate}</p>
-        //     </div>
-        //     {go && (
-        //       <NavLink to="/transactions" style={{ textDecoration: 'none' }}>
-        //         <button
-        //           type="button"
-        //           className="add-customer-btn cont-btn"
-        //           onClick={handleSubmit}
-        //         >
-        //           Add
-        //         </button>
-        //       </NavLink>
-        //     )}
-        //   </form>
-        // </>
-        <p>Hi</p>
+        <>
+          <div className="inner-container">
+            <h2 className="title1">Contribution details</h2>
+          </div>
+          <center style={{ color: 'crimson' }}>
+            Enter no. of days to continue!
+          </center>
+          <h3 className="details">Customer details</h3>
+          <form onSubmit={handleSubmit} className="add-customer-form">
+            <div className="contribution-container">
+              <p>Name: {name}</p>
+              <p>Daily Contribution: {daily_contribution}</p>
+              <h3 className="details t-details">Transaction Details</h3>
+              <input
+                type="number"
+                className="form-control days-input"
+                id="days_number"
+                placeholder="No. of days"
+                required
+                autoCorrect="off"
+                onChange={handleDays}
+              />
+              <input
+                type="date"
+                className="form-control days-input"
+                name=""
+                id=""
+                onChange={(e) => setTrDate(e.target.value)}
+              />
+              {/* <p>Amount: NGN {amount}</p>
+              <p>Previous payment date: {lastDate}</p>
+              <p>Current payment date: {currentDate}</p> */}
+            </div>
+            {go && (
+              <NavLink to="/transactions" style={{ textDecoration: 'none' }}>
+                <button
+                  type="button"
+                  className="add-customer-btn cont-btn"
+                  onClick={handleSubmit}
+                >
+                  Add
+                </button>
+              </NavLink>
+            )}
+          </form>
+        </>
       ) : (
         <>
           <Loader />
