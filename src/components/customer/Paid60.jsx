58137cca1a513a6d0805917921165b6153623f16
Atsighi Bright <brytebee@gmail.com>
2022-11-02 23:09:36 +0100

    :lipstick: Update Paid60.jsx
    Rendered the list

--- src/components/customer/Paid60.jsx
+++ src/components/customer/Paid60.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 import { getPercentageFromApi } from '../../redux/forms/percentagesReducer';
 
 const PaidSixty = () => {
@@ -9,15 +10,21 @@ const PaidSixty = () => {
   const { user } = data || {};
   const admins = user.role === 'admin' || user.role === 'superadmin';
 
-  console.log(percents);
-
   useEffect(() => {
     dispatch(getPercentageFromApi());
   }, []);
 
   return (
     <div>
-      {admins && <p>Paid60</p>}
+      {admins && (
+        <>
+          {percents.sixty.map((per) => (
+            <NavLink key={per.id} to={`/customers/${per.id}`}>
+              <p style={{ color: 'black' }}>{per.name}</p>
+            </NavLink>
+          ))}
+        </>
+      )}
       {!admins && <p>Unauthorized to see this page!</p>}
     </div>
   );
