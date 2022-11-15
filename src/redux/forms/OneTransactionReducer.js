7a52e95c987acd7c405d1545e97f6ad3fca38c9b
Atsighi Bright <brytebee@gmail.com>
2022-11-15 01:22:15 +0100

    :rotating_light: Linters
    Fixed

--- src/redux/forms/OneTransactionReducer.js
+++ src/redux/forms/OneTransactionReducer.js
@@ -1,20 +1,14 @@
 import axios from 'axios';
 import endpoint from '../../assets/url/url';
 
-const GET_ONE_TRANSACTION =
-  'src/redux/transactionreducer/get_one_transaction'.toUpperCase();
-const FAILED_GET_ONE_TRANSACTION =
-  'src/redux/transactionreducer/failed_get_one_transaction'.toUpperCase();
-const GET_ONE_TRANSACTION_REQUEST =
-  'src/redux/transactionreducer/get_one_transaction_request'.toUpperCase();
-const DEL_ONE_TRANS_REQUEST =
-  'src/redux/onetransactionreducer/del_one_customer_request'.toUpperCase();
+const GET_ONE_TRANSACTION = 'src/redux/transactionreducer/get_one_transaction'.toUpperCase();
+const FAILED_GET_ONE_TRANSACTION = 'src/redux/transactionreducer/failed_get_one_transaction'.toUpperCase();
+const GET_ONE_TRANSACTION_REQUEST = 'src/redux/transactionreducer/get_one_transaction_request'.toUpperCase();
+const DEL_ONE_TRANS_REQUEST = 'src/redux/onetransactionreducer/del_one_customer_request'.toUpperCase();
 
-const DEL_ONE_TRANSACTION =
-  'src/redux/onecustomerreducer/del_one_customer'.toUpperCase();
+const DEL_ONE_TRANSACTION = 'src/redux/onecustomerreducer/del_one_customer'.toUpperCase();
 
-const FAILED_DEL_ONE_CUSTOMER =
-  'src/redux/onecustomerreducer/failed_del_one_customer'.toUpperCase();
+const FAILED_DEL_ONE_CUSTOMER = 'src/redux/onecustomerreducer/failed_del_one_customer'.toUpperCase();
 
 const delOneTransDataFailed = (payload) => ({
   type: FAILED_DEL_ONE_CUSTOMER,
@@ -87,7 +81,7 @@ const OneTransactionReducer = (
     loading: false,
     error: null,
   },
-  action
+  action,
 ) => {
   switch (action.type) {
     case GET_ONE_TRANSACTION:
