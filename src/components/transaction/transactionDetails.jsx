8516e59a00839882f27d58409757a889f4e3acd5
Atsighi Bright <brytebee@gmail.com>
2022-11-21 18:56:55 +0100

    :art: Update transactionDetails.jsx
    Remove transaction fragments

--- src/components/transaction/transactionDetails.jsx
+++ src/components/transaction/transactionDetails.jsx
@@ -1,32 +1,25 @@
+/* eslint-disable */
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import Moment from 'moment';
 import '../customer/Customer.css';
-/* eslint-disable */
 import {
   delOneTransFromApi,
   getOneTransactionFromApi,
 } from '../../redux/forms/OneTransactionReducer';
 import { getOneCustomerFromApi } from '../../redux/forms/OneCustomerReducer';
 import Loader from '../loader/Loader';
-/* eslint-enable */
 
 const Transaction = () => {
   const dispatch = useDispatch();
   const param = useParams();
   const { id } = param;
-  const transaction = useSelector((state) => state.oneTransaction);
   const customer = useSelector((state) => state.oneCustomer);
-  const cardNum = JSON.parse(localStorage.getItem('cardNumber'));
-  const transCusId = transaction?.data?.customer_id;
-  const customerId = cardNum ?? transCusId;
   const data = JSON.parse(localStorage.getItem('user'));
   const { user } = data || {};
   const admins = user.role === 'admin' || user.role === 'superadmin';
-  const data1 = Object.keys(transaction.data).length >= 1;
-  const data2 = Object.keys(customer.data).length >= 1;
-  const isReady = data1 && data2;
+  const isReady = Object.keys(customer.data).length >= 1;
   const markId = localStorage.getItem('_id');
 
   useEffect(() => {
@@ -34,7 +27,6 @@ const Transaction = () => {
     dispatch(getOneTransactionFromApi(id));
   }, []);
 
-  /* eslint-disable */
   const comma = (num) => {
     const number = parseInt(num);
     const newText = number.toLocaleString();
