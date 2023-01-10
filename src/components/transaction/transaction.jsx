90ca237bbb0f7f799e9d64a5cefbb2cbc8e586b2
Atsighi Bright <brytebee@gmail.com>
2023-01-10 12:30:34 +0100

    Update transaction.jsx styles

--- src/components/transaction/transaction.jsx
+++ src/components/transaction/transaction.jsx
@@ -20,6 +20,8 @@ const Transaction = () => {
     dispatch(getOneTransactionFromApi(paramID));
   }, []);
 
+  console.log(transaction);
+
   const comma = (num) => {
     const number = parseInt(num);
     const newText = number.toLocaleString();
@@ -52,50 +54,51 @@ const Transaction = () => {
   );
 
   return (
-    <div className="containa transaction">
-      {isReady ? (
-        <div className="tansaction-del-container">
-          <div className="details-container">
-            <p className="cus-details">
-              <span>Previous contribution date:</span>{' '}
-              {previous_contribution_date}
-            </p>
-            <p className="cus-details">
-              <span>Current contribution date:</span>{' '}
-              {current_contribution_date}
-            </p>
-            <p className="cus-details">
-              <span>Days paid for:</span> {days_paid_for}
-            </p>
-            <p className="cus-details">
-              <span>Transaction No. </span>
-              {/* {user.location_area.slice(0, 3).toUpperCase()} */}
-              TRN{user.id}/{transaction.data.id}
-            </p>
-            <p className="cus-details">
-              <span>Payment Date: </span>
-              {Moment(transaction_date).format('MMMM DD, LT')}
-            </p>
-            <p className="cus-details">
-              <span>Amount:</span> {`NGN ${comma(amount)}`}
-            </p>
-            <p className="cus-details">
-              <span>Date of transaction: </span>
-              {Moment(created_at).format('MMMM DD, LT')}
-            </p>
-          </div>
-          <div className="del">
-            <NavLink to="/transactiondetails">
-              <button type="button" className="view-trans">
-                More Details
-              </button>
-            </NavLink>
+    <>
+      {transaction.loading && <Loader />}
+      {!transaction.loading && (
+        <div className="containa transaction">
+          <div className="tansaction-del-container">
+            <div className="details-container">
+              <p className="cus-details">
+                <span>Previous contribution date:</span>{' '}
+                {previous_contribution_date}
+              </p>
+              <p className="cus-details">
+                <span>Current contribution date:</span>{' '}
+                {current_contribution_date}
+              </p>
+              <p className="cus-details">
+                <span>Days paid for:</span> {days_paid_for}
+              </p>
+              <p className="cus-details">
+                <span>Transaction No. </span>
+                {/* {user.location_area.slice(0, 3).toUpperCase()} */}
+                TRN{user.id}/{transaction.data.id}
+              </p>
+              <p className="cus-details">
+                <span>Payment Date: </span>
+                {Moment(transaction_date).format('MMMM DD, LT')}
+              </p>
+              <p className="cus-details">
+                <span>Amount:</span> {`NGN ${comma(amount)}`}
+              </p>
+              <p className="cus-details">
+                <span>Date of transaction: </span>
+                {Moment(created_at).format('MMMM DD, LT')}
+              </p>
+            </div>
+            <div className="del">
+              <NavLink to="/transactiondetails">
+                <button type="button" className="view-trans">
+                  More Details
+                </button>
+              </NavLink>
+            </div>
           </div>
         </div>
-      ) : (
-        <Loader />
       )}
-    </div>
+    </>
   );
 };
 
