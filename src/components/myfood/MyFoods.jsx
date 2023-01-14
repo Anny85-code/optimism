7b40c5a23921edf48a83f30fd68a2edf42a3de62
Atsighi Bright <brytebee@gmail.com>
2023-01-14 21:26:20 +0100

    :art: Update MyFoods
    Get a single customer food items from API directly to avoid any filtering

--- src/components/myfood/MyFoods.jsx
+++ src/components/myfood/MyFoods.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import { getMarketerItemsFromApi } from '../../redux/forms/marketerItemsReducer';
+import { getOneCustomerFoodFromApi } from '../../redux/forms/oneCustomerFoodReducer';
 import { getOneCustomerFromApi } from '../../redux/forms/OneCustomerReducer';
 import Loader from '../loader/Loader';
 import './Myfood.css';
@@ -11,30 +11,24 @@ const MyFoods = () => {
   const dispatch = useDispatch();
   const param = useParams();
   const customer = useSelector((state) => state.oneCustomer);
-  const myFoods = useSelector(
-    (state) => state.marketerItemsReducer?.data?.items
-  );
+  const foods = useSelector((state) => state.customerFood);
+  const food = foods?.data?.my_food;
   const customerData = customer.data;
-  const food = myFoods?.filter((food) => food.v2_customer_id == param.id);
   const { name, daily_contribution } = customerData;
-  const UserData = JSON.parse(localStorage.getItem('user'));
-  const { user } = UserData || {};
 
   useEffect(() => {
-    user.role === 'marketer'
-      ? dispatch(getMarketerItemsFromApi(user.id))
-      : dispatch(getMarketerItemsFromApi(localStorage.getItem('_id')));
+    dispatch(getOneCustomerFoodFromApi(param.id));
     dispatch(getOneCustomerFromApi(param.id));
   }, []);
 
   const comma = (num) => {
-    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    return num?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
   };
 
   return (
-    <div className="transact-customer-container">
-      {name ? (
-        <>
+    <>
+      {!customer.loading ? (
+        <div className="transact-customer-container">
           <div className="custrans-name">
             <h4 className="columns">
               <span className="cus-name-name">Name</span>
@@ -78,47 +72,51 @@ const MyFoods = () => {
             ></h3>
             <h3 className="columns" id="col"></h3>
           </div>
-          {food?.map((food) => {
-            const myFoods = Object.values(JSON.parse(food.items));
-            return (
-              <div key={food.id}>
-                {myFoods ? (
-                  myFoods?.map((myFood) => (
-                    <ul className="n-child">
-                      <li>
-                        <div key={myFood.id} className="custrans-name">
-                          <h4 className="columns"></h4>
-                          <h4 className="columns i" id="top"></h4>
-                          <h4 className="columns i">{myFood.name}</h4>
-                          <h4 className="columns i">{` NGN ${comma(
-                            myFood.price
-                          )}`}</h4>
-                          <h4
-                            className="columns i"
-                            style={{ borderRight: '2px solid crimson' }}
-                          >
-                            {myFood.qauntity}
-                          </h4>
-                          <h4 className="columns ">{` NGN ${comma(
-                            myFood.subTotal
-                          )}`}</h4>
-                        </div>
-                      </li>
-                    </ul>
-                  ))
-                ) : (
-                  <>
-                    <p className="no-transact-p">No items to show!</p>
-                  </>
-                )}
-              </div>
-            );
-          })}
-        </>
+          {!foods.loading ? (
+            food?.map((food) => {
+              const myFoods = Object.values(JSON.parse(food.items));
+              return (
+                <div key={food.id}>
+                  {myFoods ? (
+                    myFoods?.map((myFood) => (
+                      <ul className="n-child">
+                        <li>
+                          <div key={myFood.id} className="custrans-name">
+                            <h4 className="columns"></h4>
+                            <h4 className="columns i" id="top"></h4>
+                            <h4 className="columns i">{myFood.name}</h4>
+                            <h4 className="columns i">{` NGN ${comma(
+                              myFood.price
+                            )}`}</h4>
+                            <h4
+                              className="columns i"
+                              style={{ borderRight: '2px solid crimson' }}
+                            >
+                              {myFood.qauntity}
+                            </h4>
+                            <h4 className="columns ">{` NGN ${comma(
+                              myFood.subTotal
+                            )}`}</h4>
+                          </div>
+                        </li>
+                      </ul>
+                    ))
+                  ) : (
+                    <>
+                      <p className="no-transact-p">No items to show!</p>
+                    </>
+                  )}
+                </div>
+              );
+            })
+          ) : (
+            <Loader />
+          )}
+        </div>
       ) : (
         <Loader />
       )}
-    </div>
+    </>
   );
 };
 
