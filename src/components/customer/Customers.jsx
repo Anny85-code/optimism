dd395f5447802e194a5fe9dd1787f76a929c9d29
Atsighi Bright <brytebee@gmail.com>
2023-01-22 22:19:27 +0100

    :art: Update Customers.jsx
    Swapped fetching all customers to fetching a merketers customers only

--- src/components/customer/Customers.jsx
+++ src/components/customer/Customers.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { getCustomerFromApi } from '../../redux/forms/customerReducer';
+// import { getCustomerFromApi } from '../../redux/forms/customerReducer';
 import './Customers.css';
 import Loader from '../loader/Loader';
 import { getMarketerCustomersFromApi } from '../../redux/forms/marketerCustomersReducer';
@@ -9,35 +9,38 @@ import { getMarketerCustomersFromApi } from '../../redux/forms/marketerCustomers
 
 const Customers = () => {
   const dispatch = useDispatch();
-  const allCustomers = useSelector((state) => state.customer?.data?.customers);
+  // const allCustomers = useSelector((state) => state.customer?.data?.customers);
   const markCustomers = useSelector((state) => state.mCustomers);
   const data = JSON.parse(localStorage.getItem('user'));
   const { user } = data || {};
   const reId = localStorage.getItem('_id');
-  const admins = user.role === 'admin' || user.role === 'superadmin';
-  const supervisor = user.role === 'supervisor';
-  const isMarketer = user.role === 'marketer';
+  // const admins = user.role === 'admin' || user.role === 'superadmin';
+  // const supervisor = user.role === 'supervisor';
+  // const isMarketer = user.role === 'marketer';
   const sliceChunk = 10;
   const [nx, setNx] = useState(sliceChunk);
   const [pr, setPr] = useState(0);
 
   useEffect(() => {
-    dispatch(getCustomerFromApi());
-    dispatch(getMarketerCustomersFromApi(+reId));
+    // dispatch(getCustomerFromApi());
+    dispatch(getMarketerCustomersFromApi(reId ?? user.id));
+    // if (isMarketer) dispatch(getMarketerCustomersFromApi(user.id));
   }, []);
 
-  console.log(markCustomers, +reId);
+  const newData = markCustomers?.data?.customers;
 
-  const newData = allCustomers?.filter((customer) => {
-    if (
-      (admins && +reId === customer.user_id) ||
-      (supervisor && +reId === customer.user_id) ||
-      (isMarketer && user.id === customer.user_id)
-    ) {
-      return customer;
-    }
-  });
-  const len = newData?.length;
+  // const newData = allCustomers?.filter((customer) => {
+  //   if (
+  //     (admins && +reId === customer.user_id) ||
+  //     (supervisor && +reId === customer.user_id) ||
+  //     (isMarketer && user.id === customer.user_id)
+  //   ) {
+  //     return customer;
+  //   }
+  // });
+
+  console.log(newData);
+  const len = markCustomers?.data?.no;
 
   const handleNext = () => {
     if (nx <= len) {
@@ -55,7 +58,7 @@ const Customers = () => {
 
   return (
     <div>
-      {len > 0 ? (
+      {!markCustomers?.loading ? (
         <div className="transact-customer-container1">
           <div className="custrans-name1">
             <h4 className="columns">
@@ -86,29 +89,26 @@ const Customers = () => {
           </div>
 
           {newData &&
-            newData
-              .sort((a, b) => b.created_at - a.created_at)
-              .slice(pr, nx)
-              .map((customer) => (
-                <NavLink key={customer.id} to={`/customers/${customer.id}`}>
-                  <ul id="p-child">
-                    <li>
-                      <div className="custrans-name1">
-                        <h4 className="columns" style={{ color: 'crimson' }}>
-                          {customer.name}
-                        </h4>
-                        <h4
-                          className="columns i"
-                          style={{ borderRight: '2px solid crimson' }}
-                        >
-                          {customer.phone}
-                        </h4>
-                        <h4 className="columns"></h4>
-                      </div>
-                    </li>
-                  </ul>
-                </NavLink>
-              ))}
+            newData.slice(pr, nx).map((customer) => (
+              <NavLink key={customer.id} to={`/customers/${customer.id}`}>
+                <ul id="p-child">
+                  <li>
+                    <div className="custrans-name1">
+                      <h4 className="columns" style={{ color: 'crimson' }}>
+                        {customer.name}
+                      </h4>
+                      <h4
+                        className="columns i"
+                        style={{ borderRight: '2px solid crimson' }}
+                      >
+                        {customer.phone}
+                      </h4>
+                      <h4 className="columns"></h4>
+                    </div>
+                  </li>
+                </ul>
+              </NavLink>
+            ))}
         </div>
       ) : (
         <Loader />
