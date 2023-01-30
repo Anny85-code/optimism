f02a4fffee00c1902d0f5fddde7b882cca849ba1
Atsighi Bright <brytebee@gmail.com>
2023-01-30 14:44:24 +0100

    :lipstick: Teams.jsx
    Created the head section and cards of teams

--- src/components/landing/teams/Teams.jsx
+++ src/components/landing/teams/Teams.jsx
@@ -2,16 +2,58 @@
 import React from 'react';
 import { LandingFoot } from '../foot/LandingFoot';
 import LandingNav from '../nav/LandingNav';
+import './Teams.css';
+import TeamsCard from './TeamsCard';
+import Img1 from '../../../assets/image/teams_demo.jpg';
+
+const teams = [
+  {
+    id: 1,
+    name: Img1,
+    title: 'Lorem ipsum dolor sit',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est accusamus, repudiandae illum exercitationem incidunt maiores.',
+  },
+  {
+    id: 2,
+    name: Img1,
+    title: 'Lorem ipsum dolor sit',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est accusamus, repudiandae illum exercitationem incidunt maiores.',
+  },
+  {
+    id: 3,
+    name: Img1,
+    title: 'Lorem ipsum dolor sit',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est accusamus, repudiandae illum exercitationem incidunt maiores.',
+  },
+  {
+    id: 4,
+    name: Img1,
+    title: 'Lorem ipsum dolor sit',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est accusamus, repudiandae illum exercitationem incidunt maiores.',
+  },
+  {
+    id: 5,
+    name: Img1,
+    title: 'Lorem ipsum dolor sit',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est accusamus, repudiandae illum exercitationem incidunt maiores.',
+  },
+  {
+    id: 6,
+    name: Img1,
+    title: 'Lorem ipsum dolor sit',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est accusamus, repudiandae illum exercitationem incidunt maiores.',
+  },
+];
 
 const Teams = () => {
   return (
-    <div>
+    <div className="teams_container">
       <LandingNav />
-      <h2>Teams</h2>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores
-      dignissimos voluptas aperiam obcaecati ratione vero, officiis, cumque rem
-      doloribus tempora debitis eius adipisci repellat qui exercitationem animi
-      blanditiis in quasi!
+      <section className="team_head">
+        <h3>teams</h3>
+        {/* <h3>Chicken Recipes</h3> */}
+      </section>
+      <TeamsCard props={teams} />
       <LandingFoot />
     </div>
   );
---
+++ src/components/landing/teams/TeamsCard.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable */
+import React from 'react';
+import PropTypes from 'prop-types';
+import './TeamsCard.css';
+
+const TeamsCard = ({ props }) => {
+  return (
+    <div className="teams_card row">
+      {props.map((el) => (
+        <div key={el.id} className="column">
+          <img src={el.name} alt={el.name} style={{ width: '120px' }} />
+          <h4>{el.title}</h4>
+          <p>{el.desc}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+TeamsCard.propTypes = {
+  props: PropTypes.array.isRequired,
+};
+
+export default TeamsCard;
+/* eslint-enable */
