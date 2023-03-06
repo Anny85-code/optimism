e52ec15046639434251624330379ec9c444f9663
Atsighi Bright <brytebee@gmail.com>
2023-03-06 17:51:28 +0100

    :art: Update Teams.jsx
    Added variety of images for demo

--- src/components/landing/teams/Teams.jsx
+++ src/components/landing/teams/Teams.jsx
@@ -5,6 +5,11 @@ import LandingNav from '../nav/LandingNav';
 import './Teams.css';
 import TeamsCard from './TeamsCard';
 import Img1 from '../../../assets/image/teams_demo.jpg';
+import Img2 from '../../../assets/image/recpie_2.png.webp';
+import Img3 from '../../../assets/image/calls.jpg';
+import Img4 from '../../../assets/image/recpie_3.png.webp';
+import Img5 from '../../../assets/image/teams_demo.jpg';
+import Img6 from '../../../assets/image/recpie_1.png.webp';
 
 const teams = [
   {
@@ -15,31 +20,31 @@ const teams = [
   },
   {
     id: 2,
-    name: Img1,
+    name: Img2,
     title: 'Lorem ipsum dolor sit',
     desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est accusamus, repudiandae illum exercitationem incidunt maiores.',
   },
   {
     id: 3,
-    name: Img1,
+    name: Img3,
     title: 'Lorem ipsum dolor sit',
     desc: 'Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Est accusamusLorem ipsum dolor sit amet consectetur adipisicing elit. Est accusamusipsum dolor sit amet consectetur adipisicing elit. Est accusamus, repudiandae illum exercitationem incidunt maiores.',
   },
   {
     id: 4,
-    name: Img1,
+    name: Img4,
     title: 'Lorem ipsum dolor sit',
     desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est accusamus, repudiandae illum exercitationem incidunt maiores.',
   },
   {
     id: 5,
-    name: Img1,
+    name: Img5,
     title: 'Lorem ipsum dolor sit',
     desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est accusamus, repudiandae illum exercitationem incidunt maiores.',
   },
   {
     id: 6,
-    name: Img1,
+    name: Img6,
     title: 'Lorem ipsum dolor sit',
     desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est accusamus, Lorem ipsum dolor sit amet consectetur adipisicing elit. Est accusamusrepudiandae illum exercitationem incidunt maiores.',
   },
