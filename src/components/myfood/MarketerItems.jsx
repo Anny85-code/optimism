7c5e80df5ec43458dd88e8573d6977a43a066273
Atsighi Bright <brytebee@gmail.com>
2023-03-22 10:08:17 +0100

    Fixed marketer items's empty page

--- src/components/myfood/MarketerItems.jsx
+++ src/components/myfood/MarketerItems.jsx
@@ -1,11 +1,23 @@
 /* eslint-disable */
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { getMarketerItemsFromApi } from '../../redux/forms/marketerItemsReducer';
 import '../items/products/Itemstat.css';
 import Loader from '../loader/Loader';
 import RenderItems from './RenderItems';
 
 const MarketerItems = () => {
   const mFoods = useSelector((state) => state.marketerItemsReducer);
+  const dispatch = useDispatch();
+  const param = useParams();
+  const { id } = param;
+
+  console.log(mFoods);
+
+  useEffect(() => {
+    dispatch(getMarketerItemsFromApi(id));
+  }, []);
 
   return (
     <div>{mFoods?.loading ? <Loader /> : <RenderItems mFoods={mFoods} />}</div>
