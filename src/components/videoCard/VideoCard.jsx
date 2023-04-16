87b86ab20f95c483e8c3119cee145d05ff552df6
uae <aniekaudo@yahoo.com>
2023-04-16 10:49:18 +0100

    :art: Landing nav
    Update landig nav videocard

--- src/components/videoCard/VideoCard.jsx
+++ src/components/videoCard/VideoCard.jsx
@@ -34,9 +34,9 @@ const VideoCard = () => {
                 height="315"
                 src="https://www.youtube.com/embed/BuhrHclfW8o"
                 title="YouTube video player"
-                frameborder="0"
+                frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                allowfullscreen
+                allowFullScreen
               ></iframe>
             </div>
           </li>
