99b4af860d7096a59189dc8b1635adfdaefbc521
Atsighi Bright <brytebee@gmail.com>
2023-10-18 16:22:50 +0100

    Update the MarketerPercent component

--- src/components/customer/MarketerPercent.jsx
+++ src/components/customer/MarketerPercent.jsx
@@ -4,25 +4,34 @@ import { useParams } from 'react-router-dom';
 import { getMarketerPercentFromApi } from '../../redux/forms/marketerPercentReducer';
 import Loader from '../loader/Loader';
 import RenderPercent from './RenderPercent';
+import { getSupervisorPercentFromApi } from '../../redux/forms/supervisorPercentReducer';
 
 const MarketerPercent = () => {
   const dispatch = useDispatch();
   const params = useParams();
   const percents = useSelector((state) => state.mPercent);
+  const supervisors = useSelector((state) => state.supvPercent);
+  const usersRed = useSelector((state) => state.userManReducer);
+  const users = usersRed?.data;
   const { id } = params;
+  const currentUser = users?.filter((user) => user?.id === +id);
+  const role = currentUser?.[0]?.role;
+  const sup = role === 'supervisor';
+  const loading = !percents?.data || !supervisors?.data;
 
   useEffect(() => {
     dispatch(getMarketerPercentFromApi(id));
+    dispatch(getSupervisorPercentFromApi(id));
   }, []);
 
   return (
     <div>
-      {percents.loading === true ? (
+      {loading ? (
         <Loader />
       ) : (
         <RenderPercent
-          percents={percents?.data}
-          owner={percents?.data?.marketer}
+          percents={sup ? supervisors?.data : percents?.data}
+          owner={sup ? supervisors?.data?.marketer : percents?.data?.marketer}
         />
       )}
     </div>
