ed47d364c592a89e35f48ca856f641a34ef041a4
Bright Atsighi <brytebee@gmail.com>
2024-03-03 19:33:49 +0100

    Connected all new pages to BE

--- src/components/latest-collect/LatestCollect.jsx
+++ src/components/latest-collect/LatestCollect.jsx
@@ -1,42 +1,58 @@
 /* eslint-disable */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import endpoint from '../../assets/url/url';
 import './LatestCollect.css';
+import Loader from '../loader/Loader';
 
 const LatestCollect = () => {
-  const data = {
-    total: 8,
-    marketer: 'BLESSING YAHAYA',
-    data: {
-      entry_date: '2024-03-02',
-      collected_for: {
-        latest: '2024-02-28',
-        oldest: '2024-02-28',
-      },
-    },
-  };
+  const [data, setData] = useState({});
+  const { id } = useParams();
+  const { token } = localStorage;
+
+  useEffect(() => {
+    (async () => {
+      const fetchData = await fetch(
+        `${endpoint}/users/${id}/marketer_last_collect`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const res = await fetchData.json();
+      setData(res);
+    })();
+  }, []);
 
   return (
-    <div className="transact-customer-container">
-      <div>
-        <table>
-          <thead className="table-head">
-            <td> Marketer: {data.marketer}</td>
+    <>
+      {!data.data && <Loader />}
 
-            <td> Total: {data.total}</td>
-          </thead>
-          <thead>
-            <td>Entry Date</td>
-            <td>Latest</td>
-            <td>Oldest</td>
-          </thead>
-          <tbody>
-            <td>{data.data.entry_date}</td>
-            <td>{data.data.collected_for.latest}</td>
-            <td>{data.data.collected_for.oldest}</td>
-          </tbody>
-        </table>
-      </div>
-    </div>
+      {data.data && (
+        <div className="transact-customer-container">
+          <div>
+            <table>
+              <thead className="table-head">
+                <td> Marketer: {data.marketer}</td>
+
+                <td> Total: {data.total}</td>
+              </thead>
+              <thead>
+                <td>Entry Date</td>
+                <td>Latest</td>
+                <td>Oldest</td>
+              </thead>
+              <tbody>
+                <td>{data.data.entry_date}</td>
+                <td>{data.data.collected_for.latest}</td>
+                <td>{data.data.collected_for.oldest}</td>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
