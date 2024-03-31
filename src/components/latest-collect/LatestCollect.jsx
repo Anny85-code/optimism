dac9703f6631f91d8549a7b72ecc35d0d9268a61
Bright Atsighi <brytebee@gmail.com>
2024-03-31 16:33:19 +0100

    Rendered latest collection

--- src/components/latest-collect/LatestCollect.jsx
+++ src/components/latest-collect/LatestCollect.jsx
@@ -4,11 +4,36 @@ import { useParams } from 'react-router-dom';
 import endpoint from '../../assets/url/url';
 import './LatestCollect.css';
 import Loader from '../loader/Loader';
+import { useSelector } from 'react-redux';
 
 const LatestCollect = () => {
+  const getCustomers = useSelector((state) => state.customer?.data?.customers);
   const [data, setData] = useState({});
   const { id } = useParams();
   const { token } = localStorage;
+  const sliceChunk = 50;
+  const [nx, setNx] = useState(sliceChunk);
+  const [pr, setPr] = useState(0);
+  const len = data?.total;
+
+  const handleNext = () => {
+    if (nx <= len) {
+      setNx(nx + sliceChunk);
+      setPr(pr + sliceChunk);
+    }
+  };
+
+  const handPrevious = () => {
+    if (pr > 1) {
+      setPr(pr - sliceChunk);
+      setNx(nx - sliceChunk);
+    }
+  };
+
+  const cusName = (id) => {
+    const cus = getCustomers?.filter((cus) => cus.id === id);
+    return cus?.[0].name;
+  };
 
   useEffect(() => {
     (async () => {
@@ -35,21 +60,44 @@ const LatestCollect = () => {
             <table>
               <thead className="table-head">
                 <td> Marketer: {data.marketer}</td>
-
-                <td> Total: {data.total}</td>
+                <td> Total: {data?.total}</td>
               </thead>
               <thead>
+                <td>Customer</td>
+                <td>Amount</td>
                 <td>Entry Date</td>
-                <td>Latest</td>
-                <td>Oldest</td>
+                <td>Coll. Date</td>
               </thead>
-              <tbody>
-                <td>{data.data.entry_date}</td>
-                <td>{data.data.collected_for.latest}</td>
-                <td>{data.data.collected_for.oldest}</td>
-              </tbody>
+              {data.data
+                .sort((a, b) => b.transaction_date - a.transaction_date)
+                .slice(pr, nx)
+                .map((trans, i) => (
+                  <tbody key={i}>
+                    <td>{cusName(trans.v2_customer_id)}</td>
+                    <td>{trans.amount}</td>
+                    <td>{trans.created_at}</td>
+                    <td>{trans.transaction_date}</td>
+                  </tbody>
+                ))}
             </table>
           </div>
+          {len > 0 && (
+            <div className="pre-next-cont">
+              <i
+                className="fa fa-caret-left fa-2x text-red"
+                onClick={handPrevious}
+                style={{ cursor: 'pointer' }}
+              />
+              <p className="pre-text">
+                {pr + 1} - {nx < len ? nx : len} <span>of</span> {len}
+              </p>
+              <i
+                className="fa fa-caret-right fa-2x text-red"
+                onClick={handleNext}
+                style={{ cursor: 'pointer' }}
+              />
+            </div>
+          )}
         </div>
       )}
     </>
