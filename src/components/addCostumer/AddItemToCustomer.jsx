8ec94f4058e3ba3d39a4b0ba464eb5a4e1f46dd1
Bright Atsighi <brytebee@gmail.com>
2024-08-18 15:36:48 +0100

    Update the currency

--- src/components/addCostumer/AddItemToCustomer.jsx
+++ src/components/addCostumer/AddItemToCustomer.jsx
@@ -27,7 +27,7 @@ const AddItemToCustomer = () => {
   const getFormattedPrice = (price) =>
     new Intl.NumberFormat('en-IN', {
       style: 'currency',
-      currency: '₦',
+      currency: 'NGN',
     }).format(price);
 
   const handleGoTo = () => {
